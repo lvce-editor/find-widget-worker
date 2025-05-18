3511940703dc20261ffddae35f302a46f6ee76dc
Le Vivilet <levivlet@pm.me>
2025-05-18 10:30:26 +0200

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -5,4 +5,12 @@ export default [
   {
     ignores: ['**/server/**', '**/e2e/**', '**/memory/**'],
   },
+  {
+    rules: {
+      'no-constant-condition': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+      'no-useless-escape': 'off',
+    },
+  },
 ]

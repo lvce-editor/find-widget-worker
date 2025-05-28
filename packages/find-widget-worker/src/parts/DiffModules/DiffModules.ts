051add99ddc2ca99ddcebd54a03eaf62e485ce15
Le Vivilet <levivlet@pm.me>
2025-05-28 19:46:44 +0200

    feature: add diff modules

--- packages/find-widget-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/find-widget-worker/src/parts/DiffModules/DiffModules.ts
@@ -1,3 +1,10 @@
-export const modules: readonly any[] = []
+import * as DiffBounds from '../DiffBounds/DiffBounds.ts'
+import * as DiffEventListeners from '../DiffEventListeners/DiffEventListeners.ts'
+import * as DiffFocusContext from '../DiffFocusContext/DiffFocusContext.ts'
+import * as DiffItems from '../DiffItems/DiffItems.ts'
+import * as DiffParentUid from '../DiffParentUid/DiffParentUid.ts'
+import * as DiffType from '../DiffType/DiffType.ts'
 
-export const numbers: readonly number[] = []
+export const modules = [DiffEventListeners.isEqual, DiffItems.isEqual, DiffBounds.isEqual, DiffParentUid.isEqual, DiffFocusContext.isEqual]
+
+export const numbers = [DiffType.RenderEventListeners, DiffType.RenderItems, DiffType.RenderBounds, DiffType.RenderUid, DiffType.RenderFocusContext]

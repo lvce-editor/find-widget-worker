38aeb0989f0d22a8f84a110c25f73bedc4d239d6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-28 19:15:30 +0200

    feature: rename commands (#6)

--- packages/find-widget-worker/src/parts/Diff2/Diff2.ts
+++ packages/find-widget-worker/src/parts/Diff2/Diff2.ts
@@ -1,8 +1,8 @@
 import * as Diff from '../Diff/Diff.ts'
-import * as ColorPickerStates from '../FindWidgetStates/FindWidgetStates.ts'
+import * as FindWidgetStates from '../FindWidgetStates/FindWidgetStates.ts'
 
 export const diff2 = (uid: number): readonly number[] => {
-  const { oldState, newState } = ColorPickerStates.get(uid)
+  const { oldState, newState } = FindWidgetStates.get(uid)
   const diffResult = Diff.diff(oldState, newState)
   return diffResult
 }
--- packages/find-widget-worker/src/parts/Render/Render.ts
+++ packages/find-widget-worker/src/parts/Render/Render.ts
@@ -1,9 +1,9 @@
 import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
 import * as Diff from '../Diff/Diff.ts'
-import * as ColorPickerStates from '../FindWidgetStates/FindWidgetStates.ts'
+import * as FindWidgetStates from '../FindWidgetStates/FindWidgetStates.ts'
 
 export const doRender = (uid: number): readonly any[] => {
-  const { oldState, newState } = ColorPickerStates.get(uid)
+  const { oldState, newState } = FindWidgetStates.get(uid)
   const diffResult = Diff.diff(oldState, newState)
   const commands = ApplyRender.applyRender(oldState, newState, diffResult)
   return commands

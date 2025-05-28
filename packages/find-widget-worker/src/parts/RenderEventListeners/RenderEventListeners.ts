97baa4c7a07bb3f1d68cf57f83d819b6fea6b076
Le Vivilet <levivlet@pm.me>
2025-05-28 20:29:55 +0200

    tsc

--- packages/find-widget-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
+++ packages/find-widget-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
@@ -1,9 +1,10 @@
 import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
 import { getEventListeners } from '../GetEventListeners/GetEventListeners.ts'
 import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
+import * as WidgetId from '../WidgetId/WidgetId.ts'
 
 export const renderEventListeners = (state: FindWidgetState): readonly any[] => {
   const { uid } = state
-  const eventListeners = getEventListeners(uid)
+  const eventListeners = getEventListeners(uid, WidgetId.Find)
   return [RenderMethod.SetEventListeners, uid, eventListeners]
 }

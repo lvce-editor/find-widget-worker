daec882bc94218d1c0da8a3742ac4f6c044ce098
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-28 20:30:46 +0200

    feature: add event listeners (#26)
    * feature: add event listeners
    
    * tsc

--- packages/find-widget-worker/src/parts/GetEventListeners/GetEventListeners.ts
+++ packages/find-widget-worker/src/parts/GetEventListeners/GetEventListeners.ts
@@ -1,5 +1,59 @@
 import type { DomEventListener } from '../DomEventListener/DomEventListener.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
-export const getEventListeners = (uid: number): readonly DomEventListener[] => {
-  return []
+export const getEventListeners = (uid: number, widgetId: number): readonly DomEventListener[] => {
+  return [
+    {
+      name: DomEventListenerFunctions.HandleInput,
+      params: ['executeWidgetCommand', 'FindWidget', 'FindWidget.handleInput', 0, widgetId, 'event.target.value'],
+    },
+    {
+      name: DomEventListenerFunctions.HandleClickClose,
+      params: ['executeWidgetCommand', 'FindWidget', 'FindWidget.handleClickClose', 0, widgetId],
+    },
+    {
+      name: DomEventListenerFunctions.HandleClickFocusPreviousMatch,
+      params: ['executeWidgetCommand', 'FindWidget', 'FindWidget.handleClickPreviousMatch', 0, widgetId],
+    },
+    {
+      name: DomEventListenerFunctions.HandleClickNextMatch,
+      params: ['executeWidgetCommand', 'FindWidget', 'FindWidget.handleClickNextMatch', 0, widgetId],
+    },
+    {
+      name: DomEventListenerFunctions.HandleClickReplace,
+      params: ['executeWidgetCommand', 'FindWidget', 'FindWidget.handleClickReplace', 0, widgetId],
+    },
+    {
+      name: DomEventListenerFunctions.HandleClickReplace,
+      params: ['executeWidgetCommand', 'FindWidget', 'FindWidget.handleClickReplaceAll', 0, widgetId],
+    },
+    {
+      name: DomEventListenerFunctions.HandleClickToggleReplace,
+      params: ['executeWidgetCommand', 'FindWidget', 'FindWidget.handleClickToggleReplace', 0, widgetId],
+    },
+    {
+      name: DomEventListenerFunctions.HandleBlur,
+      params: ['executeWidgetCommand', 'FindWidget', 'FindWidget.handleBlur', 0, widgetId],
+    },
+    {
+      name: DomEventListenerFunctions.HandleReplaceInput,
+      params: ['executeWidgetCommand', 'FindWidget', 'FindWidget.handleReplaceInput', 0, widgetId],
+    },
+    {
+      name: DomEventListenerFunctions.HandleReplaceFocus,
+      params: ['executeWidgetCommand', 'FindWidget', 'FindWidget.handleReplaceFocus', 0, widgetId],
+    },
+    {
+      name: DomEventListenerFunctions.HandleFocusPrevious,
+      params: ['executeWidgetCommand', 'FindWidget', 'FindWidget.handleFocusPrevious', 0, widgetId],
+    },
+    {
+      name: DomEventListenerFunctions.HandleFocusNext,
+      params: ['executeWidgetCommand', 'FindWidget', 'FindWidget.handleFocusNext', 0, widgetId],
+    },
+    {
+      name: DomEventListenerFunctions.HandleClickReplaceAll,
+      params: ['executeWidgetCommand', 'FindWidget', 'FindWidget.handleClickReplaceAll', 0, widgetId],
+    },
+  ]
 }

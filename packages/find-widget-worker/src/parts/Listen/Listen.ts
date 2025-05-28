92ffb2b841ca0e5774f50b43ced99478c31369d4
Le Vivilet <levivlet@pm.me>
2025-05-28 19:35:49 +0200

    feature: add editor worker connection

--- packages/find-widget-worker/src/parts/Listen/Listen.ts
+++ packages/find-widget-worker/src/parts/Listen/Listen.ts
@@ -1,11 +1,10 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
-import * as RpcId from '../RpcId/RpcId.ts'
-import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
+import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
 
 export const listen = async (): Promise<void> => {
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
-  RpcRegistry.set(RpcId.RendererWorker, rpc)
+  EditorWorker.set(rpc)
 }
--- packages/find-widget-worker/src/parts/RendererWorker/RendererWorker.ts
+++
@@ -1,9 +0,0 @@
-import * as Rpc from '../Rpc/Rpc.ts'
-
-export const invoke = async (method: string, ...params: readonly any[]): Promise<any> => {
-  return Rpc.invoke('WebView.compatRendererWorkerInvoke', method, ...params)
-}
-
-export const invokeAndTransfer = async (method: string, ...params: readonly any[]): Promise<any> => {
-  return Rpc.invokeAndTransfer('WebView.compatRendererWorkerInvokeAndTransfer', method, ...params)
-}

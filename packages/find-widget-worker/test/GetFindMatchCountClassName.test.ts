03426fda0c5da982b0181ad6d240543d48bcf940
Le Vivilet <levivlet@pm.me>
2025-05-29 22:03:12 +0200

    feature: add find widget match count test

---
+++ packages/find-widget-worker/test/GetFindMatchCountClassName.test.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@jest/globals'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as GetFindMatchCountClassName from '../src/parts/GetFindMatchCountClassName/GetFindMatchCountClassName.ts'
+import * as MergeClassNames from '../src/parts/MergeClassNames/MergeClassNames.ts'
+
+test('getFindMatchCountClassName - with value and zero matches', () => {
+  const result = GetFindMatchCountClassName.getFindMatchCountClassName(0, 'test')
+  expect(result).toBe(MergeClassNames.mergeClassNames(ClassNames.FindWidgetMatchCount, ClassNames.FindWidgetMatchCountEmpty))
+})
+
+test('getFindMatchCountClassName - with value and non-zero matches', () => {
+  const result = GetFindMatchCountClassName.getFindMatchCountClassName(5, 'test')
+  expect(result).toBe(ClassNames.FindWidgetMatchCount)
+})
+
+test('getFindMatchCountClassName - with empty value and zero matches', () => {
+  const result = GetFindMatchCountClassName.getFindMatchCountClassName(0, '')
+  expect(result).toBe(ClassNames.FindWidgetMatchCount)
+})

791b200494d8a25d90ea821c7ede3016785605d4
Le Vivilet <levivlet@pm.me>
2025-05-29 22:41:57 +0200

    feature: add find dom test

---
+++ packages/find-widget-worker/test/GetFindWidgetVirtualDom.test.ts
@@ -0,0 +1,101 @@
+import { test, expect } from '@jest/globals'
+import * as GetFindWidgetVirtualDom from '../src/parts/GetFindWidgetVirtualDom/GetFindWidgetVirtualDom.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+import * as AriaRoles from '../src/parts/AriaRoles/AriaRoles.ts'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+
+test('getFindWidgetVirtualDom - collapsed', () => {
+  const findButtons = [
+    {
+      label: 'Find',
+      icon: 'find-icon',
+      disabled: false,
+      name: 'find-button',
+      onClick: 'handleFind',
+    },
+  ]
+
+  const result = GetFindWidgetVirtualDom.getFindWidgetVirtualDom('0 matches', false, findButtons, [], false, false, false, 0, '')
+
+  expect(result[0]).toEqual({
+    type: VirtualDomElements.Div,
+    className: 'Viewlet ViewletFind ViewletFindWidget FindWidget',
+    childCount: 2,
+    role: AriaRoles.Group,
+  })
+
+  expect(result[1]).toEqual({
+    type: VirtualDomElements.Button,
+    className: 'IconButton SearchToggleButton ',
+    title: 'Toggle Replace',
+    ariaLabel: 'Toggle Replace',
+    childCount: 1,
+    onClick: 'handleClickToggleReplace',
+    name: 'ToggleReplace',
+    ariaExpanded: false,
+    'data-command': 'toggleReplace',
+    onFocus: 'handleToggleReplaceFocus',
+  })
+
+  expect(result[2]).toEqual({
+    childCount: 0,
+    className: 'MaskIcon MaskIconChevronRight',
+    type: 4,
+  })
+
+  expect(result[3]).toEqual({
+    type: VirtualDomElements.Div,
+    className: ClassNames.FindWidgetRight,
+    childCount: 1,
+  })
+})
+
+test('getFindWidgetVirtualDom - expanded', () => {
+  const findButtons = [
+    {
+      label: 'Find',
+      icon: 'find-icon',
+      disabled: false,
+      name: 'find-button',
+      onClick: 'handleFind',
+    },
+  ]
+
+  const replaceButtons = [
+    {
+      label: 'Replace',
+      icon: 'replace-icon',
+      disabled: false,
+      name: 'replace-button',
+      onClick: 'handleReplace',
+    },
+  ]
+
+  const result = GetFindWidgetVirtualDom.getFindWidgetVirtualDom('0 matches', true, findButtons, replaceButtons, false, false, false, 0, '')
+
+  expect(result[0]).toEqual({
+    type: VirtualDomElements.Div,
+    className: 'Viewlet ViewletFind ViewletFindWidget FindWidget',
+    childCount: 2,
+    role: AriaRoles.Group,
+  })
+
+  expect(result[1]).toEqual({
+    type: VirtualDomElements.Button,
+    className: 'IconButton SearchToggleButton SearchToggleButtonExpanded',
+    title: 'Toggle Replace',
+    ariaLabel: 'Toggle Replace',
+    childCount: 1,
+    onClick: 'handleClickToggleReplace',
+    name: 'ToggleReplace',
+    ariaExpanded: true,
+    'data-command': 'toggleReplace',
+    onFocus: 'handleToggleReplaceFocus',
+  })
+
+  expect(result[3]).toEqual({
+    type: VirtualDomElements.Div,
+    className: ClassNames.FindWidgetRight,
+    childCount: 2,
+  })
+})

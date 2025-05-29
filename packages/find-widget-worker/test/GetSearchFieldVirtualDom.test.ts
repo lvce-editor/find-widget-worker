0e8aa498317455ca74209185753f2f889baa5512
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-29 22:22:33 +0200

    feature: add dom test (#52)
    * feature: add dom test
    
    * test
    
    * lint
    
    * lint

---
+++ packages/find-widget-worker/test/GetSearchFieldVirtualDom.test.ts
@@ -0,0 +1,74 @@
+import { test, expect } from '@jest/globals'
+import * as AriaRoles from '../src/parts/AriaRoles/AriaRoles.js'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.js'
+import * as GetSearchFieldVirtualDom from '../src/parts/GetSearchFieldVirtualDom/GetSearchFieldVirtualDom.js'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.js'
+
+test('getSearchFieldVirtualDom returns correct virtual dom elements', () => {
+  const name = 'test-name'
+  const placeholder = 'test-placeholder'
+  const onInput = 'test-onInput'
+  const onFocus = 'test-onFocus'
+  const insideButtons = [
+    {
+      icon: 'TestIcon',
+      checked: false,
+      title: 'Test Title',
+    },
+  ]
+  const outsideButtons: any[] = []
+
+  const result = GetSearchFieldVirtualDom.getSearchFieldVirtualDom(name, placeholder, onInput, insideButtons, outsideButtons, onFocus)
+
+  expect(result).toEqual([
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.SearchField,
+      role: AriaRoles.None,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.TextArea,
+      className: ClassNames.MultilineInputBox,
+      spellcheck: false,
+      autocapitalize: 'off',
+      autocorrect: 'off',
+      placeholder,
+      name,
+      onInput,
+      onFocus,
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.SearchFieldButtons,
+      childCount: insideButtons.length,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.SearchFieldButton,
+      title: 'Test Title',
+      role: AriaRoles.CheckBox,
+      ariaChecked: false,
+      tabIndex: 0,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: `${ClassNames.MaskIcon} TestIcon`,
+      childCount: 0,
+    },
+  ])
+})
+
+test('getSearchFieldVirtualDom throws error when outsideButtons are provided', () => {
+  const name = 'test-name'
+  const placeholder = 'test-placeholder'
+  const onInput = 'test-onInput'
+  const insideButtons: any[] = []
+  const outsideButtons = [{ icon: 'TestIcon' }]
+
+  expect(() => {
+    GetSearchFieldVirtualDom.getSearchFieldVirtualDom(name, placeholder, onInput, insideButtons, outsideButtons)
+  }).toThrow('outsideButtons are deprecated')
+})
---
+++ packages/find-widget-worker/test/GetSearchToggleButtonVirtualDom.test.ts
@@ -0,0 +1,50 @@
+import { test, expect } from '@jest/globals'
+import * as GetSearchToggleButtonVirtualDom from '../src/parts/GetSearchToggleButtonVirtualDom/GetSearchToggleButtonVirtualDom.js'
+import * as InputName from '../src/parts/InputName/InputName.js'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.js'
+
+test('getSearchToggleButtonVirtualDom returns correct virtual dom elements for collapsed state', () => {
+  const result = GetSearchToggleButtonVirtualDom.getSearchToggleButtonVirtualDom(false)
+  expect(result).toEqual([
+    {
+      type: VirtualDomElements.Button,
+      className: 'IconButton SearchToggleButton ',
+      title: 'Toggle Replace',
+      ariaLabel: 'Toggle Replace',
+      ariaExpanded: false,
+      name: InputName.ToggleReplace,
+      childCount: 1,
+      'data-command': 'toggleReplace',
+      onClick: '',
+      onFocus: 'handleToggleReplaceFocus',
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: 'MaskIcon MaskIconChevronRight',
+      childCount: 0,
+    },
+  ])
+})
+
+test('getSearchToggleButtonVirtualDom returns correct virtual dom elements for expanded state', () => {
+  const result = GetSearchToggleButtonVirtualDom.getSearchToggleButtonVirtualDom(true, 'test-onClick')
+  expect(result).toEqual([
+    {
+      type: VirtualDomElements.Button,
+      className: 'IconButton SearchToggleButton SearchToggleButtonExpanded',
+      title: 'Toggle Replace',
+      ariaLabel: 'Toggle Replace',
+      ariaExpanded: true,
+      name: InputName.ToggleReplace,
+      childCount: 1,
+      'data-command': 'toggleReplace',
+      onClick: 'test-onClick',
+      onFocus: 'handleToggleReplaceFocus',
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: 'MaskIcon MaskIconChevronDown',
+      childCount: 0,
+    },
+  ])
+})

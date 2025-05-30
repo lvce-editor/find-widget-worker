6b6a426131b5fbc5f4ab4fdc5aff6697f2bddef6
Le Vivilet <levivlet@pm.me>
2025-05-30 21:58:48 +0200

    fix value

--- packages/find-widget-worker/src/parts/RenderValue/RenderValue.ts
+++ packages/find-widget-worker/src/parts/RenderValue/RenderValue.ts
@@ -2,6 +2,6 @@ import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
 import * as InputName from '../InputName/InputName.ts'
 
 export const renderValue = (oldState: FindWidgetState, newState: FindWidgetState): readonly any[] => {
-  const { uid } = newState
-  return [/* method */ 'Viewlet.setValueByName', uid, InputName.SearchValue]
+  const { uid, value } = newState
+  return [/* method */ 'Viewlet.setValueByName', uid, InputName.SearchValue, value]
 }

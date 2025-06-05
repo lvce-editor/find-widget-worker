631d6c13014c4558a19dd942ee24bbb9741d4840
Le Vivilet <levivlet@pm.me>
2025-06-05 09:12:46 +0200

    feature: move focusNext into separate file

--- packages/e2e/src/find-widget-focus-next.ts
+++ packages/e2e/src/find-widget-focus-next.ts
@@ -1,6 +1,6 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const name = 'find-widget-open'
+export const name = 'find-widget=focus-next'
 
 export const skip = true
 
@@ -26,4 +26,23 @@ content 2`,
   const findWidgetMatchCount = Locator(`.FindWidgetMatchCount`)
   await expect(findWidgetMatchCount).toBeVisible()
   await expect(findWidgetMatchCount).toHaveText('1 of 2')
+
+  // act
+  await FindWidget.focusNext()
+
+  // assert
+  await expect(findWidgetMatchCount).toHaveText('2 of 2')
+  const editorSelection = Locator('.EditorSelection')
+  await expect(editorSelection).toHaveCSS('top', '20px')
+  await expect(editorSelection).toHaveCSS('width', /^(65|66|67|68|69).*?px$/)
+
+  // act
+  await FindWidget.setValue('content-not-found')
+
+  // assert
+  await expect(findWidgetMatchCount).toHaveText('No Results')
+  const buttonPreviousMatch = Locator('[title="Previous Match"]')
+  await expect(buttonPreviousMatch).toHaveAttribute('disabled', '')
+  const buttonNextMatch = Locator('[title="Next Match"]')
+  await expect(buttonNextMatch).toHaveAttribute('disabled', '')
 }

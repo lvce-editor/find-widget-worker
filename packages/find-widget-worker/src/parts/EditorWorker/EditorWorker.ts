6b0cc0f79fdfb50aee758fe65c6ebf26ce38f4ef
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-24 23:38:45 +0200

    feature: add dispose function (#85)
    * feature: add dispose function
    
    * update
    
    * update
    
    * memory

--- packages/find-widget-worker/src/parts/EditorWorker/EditorWorker.ts
+++ packages/find-widget-worker/src/parts/EditorWorker/EditorWorker.ts
@@ -1,3 +1,3 @@
 import { EditorWorker } from '@lvce-editor/rpc-registry'
 
-export const { invoke, set, invokeAndTransfer } = EditorWorker
+export const { invoke, set, invokeAndTransfer, dispose } = EditorWorker

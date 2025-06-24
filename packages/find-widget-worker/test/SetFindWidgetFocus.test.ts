3ee2811de721f948caf76cb335d38ba2095f9c6d
Le Vivilet <levivlet@pm.me>
2025-06-24 23:31:15 +0200

    feature: add more tests

--- packages/find-widget-worker/test/SetFindWidgetFocus.test.ts
+++ packages/find-widget-worker/test/SetFindWidgetFocus.test.ts
@@ -2,17 +2,18 @@ import { test, expect } from '@jest/globals'
 import * as CreateDefaultState from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as FocusSource from '../src/parts/FocusSource/FocusSource.ts'
 import * as SetFindWidgetFocus from '../src/parts/SetFindWidgetFocus/SetFindWidgetFocus.ts'
+import type { FindWidgetState } from '../src/parts/FindWidgetState/FindWidgetState.ts'
 
 test('setFindWidgetFocus should return same state if focus key is same', () => {
-  const state = CreateDefaultState.createDefaultState()
-  const result = SetFindWidgetFocus.setFindWidgetFocus(state, state.focus)
+  const state: FindWidgetState = CreateDefaultState.createDefaultState()
+  const result: FindWidgetState = SetFindWidgetFocus.setFindWidgetFocus(state, state.focus)
   expect(result).toBe(state)
 })
 
 test('setFindWidgetFocus should update focus and focusSource', () => {
-  const state = CreateDefaultState.createDefaultState()
-  const newFocusKey = 5
-  const result = SetFindWidgetFocus.setFindWidgetFocus(state, newFocusKey)
+  const state: FindWidgetState = CreateDefaultState.createDefaultState()
+  const newFocusKey: number = 5
+  const result: FindWidgetState = SetFindWidgetFocus.setFindWidgetFocus(state, newFocusKey)
   expect(result.focus).toBe(newFocusKey)
   expect(result.focusSource).toBe(FocusSource.Script)
 })

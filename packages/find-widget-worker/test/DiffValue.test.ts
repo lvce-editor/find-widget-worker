1c982aba0607ffaa5007b39aa2ec80df9607c649
Le Vivilet <levivlet@pm.me>
2025-09-04 15:21:40 +0200

    lint

--- packages/find-widget-worker/test/DiffValue.test.ts
+++ packages/find-widget-worker/test/DiffValue.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals'
 import type { FindWidgetState } from '../src/parts/FindWidgetState/FindWidgetState.ts'
 import * as CreateDefaultState from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as DiffValue from '../src/parts/DiffValue/DiffValue.ts'
-import * as InputSource from '../src/parts/InputSource/InputSource.ts'
+import { InputSource } from '@lvce-editor/constants'
 
 test('isEqual should return true for same values', () => {
   const oldState: FindWidgetState = CreateDefaultState.createDefaultState()

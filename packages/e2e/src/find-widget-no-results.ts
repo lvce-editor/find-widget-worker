f7e1d2cc0d2d0956090824265bc76897430014ea
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-04 14:52:05 +0200

    feature: add no results e2e test (#108)
    Co-authored-by: Le Vivilet <levivlet@pm.me>

---
+++ packages/e2e/src/find-widget-no-results.ts
@@ -0,0 +1,30 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'find-widget.no-results'
+
+export const skip = 1
+
+export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/file1.txt`,
+    `content 1
+content 2`,
+  )
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+  await Editor.setSelections(new Uint32Array([0, 0, 0, 0]))
+  await Editor.openFindWidget()
+
+  // act
+  await FindWidget.setValue('not-found')
+
+  // assert
+  const findWidgetInput = Locator('.FindWidget .MultilineInputBox')
+  await expect(findWidgetInput).toBeVisible()
+  await expect(findWidgetInput).toHaveValue('content')
+  const findWidgetMatchCount = Locator(`.FindWidgetMatchCount`)
+  await expect(findWidgetMatchCount).toBeVisible()
+  await expect(findWidgetMatchCount).toHaveText('1 of 2')
+}

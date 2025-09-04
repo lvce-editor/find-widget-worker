1c2ea822a383ff99c6bfa4d6573f59d231cdb12a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-04 23:31:28 +0200

    feature: update click handler (#156)
    * feature: updae click handler
    
    * add function
    
    * test
    
    ---------
    
    Co-authored-by: Le Vivilet <levivlet@pm.me>

---
+++ packages/find-widget-worker/test/FindWidgetToggleMatchWholeWord.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from '@jest/globals'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as FindWidgetRefreshWithEditor from '../src/parts/FindWidgetRefreshWithEditor/FindWidgetRefreshWithEditor.ts'
+import * as FindWidgetToggleMatchWholeWord from '../src/parts/FindWidgetToggleMatchWholeWord/FindWidgetToggleMatchWholeWord.ts'
+
+test('toggleMatchWholeWord - toggles and recomputes matches', () => {
+  const base = {
+    ...createDefaultState(),
+    lines: ['foo bar', 'foobar', 'foo'],
+    value: 'foo',
+  }
+  const refreshed = FindWidgetRefreshWithEditor.refresh(base, base.value, 0)
+  expect(refreshed.matchCount).toBe(3)
+  const afterToggle = FindWidgetToggleMatchWholeWord.toggleMatchWholeWord(refreshed)
+  expect(afterToggle.matchWholeWord).toBe(true)
+  expect(afterToggle.matchCount).toBe(3)
+})

613c4e50a7fdd8053427224c12ded859ec1a80e8
Le Vivilet <levivlet@pm.me>
2025-09-04 19:53:01 +0200

    feature: pass editor uid to applyEdit

--- packages/find-widget-worker/src/parts/Replace/Replace.ts
+++ packages/find-widget-worker/src/parts/Replace/Replace.ts
@@ -3,10 +3,10 @@ import { applyEdits } from '../ApplyEdits/ApplyEdits.ts'
 import { getEdits } from '../GetEdits/GetEdits.ts'
 
 export const replace = async (state: FindWidgetState): Promise<FindWidgetState> => {
-  const { lines, value, replacement } = state
+  const { lines, value, replacement, editorUid } = state
 
   const edits = getEdits(lines, value, replacement)
-  await applyEdits(edits)
+  await applyEdits(editorUid, edits)
   // TODO ask editor worker to apply edit
   // TODO ask editor worker for new lines?
   return {

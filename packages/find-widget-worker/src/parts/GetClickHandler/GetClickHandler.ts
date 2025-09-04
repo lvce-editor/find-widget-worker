f3bc439b61a2c6de203161e0df6e44261b0ff2bf
Le Vivilet <levivlet@pm.me>
2025-09-04 20:39:51 +0200

    feature; add button click handler

---
+++ packages/find-widget-worker/src/parts/GetClickHandler/GetClickHandler.ts
@@ -0,0 +1,25 @@
+import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
+import { toggleMatchCase } from '../FindWidgetToggleMatchCase/FindWidgetToggleMatchCase.ts'
+import { togglePreserveCase } from '../FindWidgetTogglePreserveCase/FindWidgetTogglePreserveCase.ts'
+import * as InputName from '../InputName/InputName.ts'
+import { replace } from '../Replace/Replace.ts'
+import { replaceAll } from '../ReplaceAll/ReplaceAll.ts'
+
+interface ClickHandler {
+  (state: FindWidgetState): FindWidgetState | Promise<FindWidgetState>
+}
+
+export const getClickHandler = (name: string): ClickHandler => {
+  switch (name) {
+    case InputName.Replace:
+      return replace
+    case InputName.ReplaceAll:
+      return replaceAll
+    case InputName.MatchCase:
+      return toggleMatchCase
+    case InputName.PreserveCase:
+      return togglePreserveCase
+    default:
+      throw new Error(`unexpected click`)
+  }
+}

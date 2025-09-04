d061498110cfb89338017f24a0dd6720aa31dd9f
Le Vivilet <levivlet@pm.me>
2025-09-04 19:13:43 +0200

    feature: enable openReplace e2e test

---
+++ packages/e2e/src/find-widget-open-replace.ts
@@ -0,0 +1,26 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.editor-open-replace'
+
+export const test: Test = async ({ Command, FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/file1.txt`,
+    `content 1
+content 2`,
+  )
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+  await Editor.setSelections(new Uint32Array([0, 0, 0, 7]))
+  await Editor.openFindWidget()
+
+  // act
+  await Command.execute(`FindWidget.toggleReplace`)
+
+  // assert
+  const toggleReplace = Locator('.FindWidget [name="ToggleReplace"]')
+  await expect(toggleReplace).toHaveAttribute(`aria-expanded`, `true`)
+  const replace = Locator(`.FindWidget .FindWidgetReplace`)
+  await expect(replace).toBeVisible()
+}

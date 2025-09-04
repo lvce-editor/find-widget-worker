c855e8d5d00fe0e4646f60719e56596551ba2ce2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-04 15:16:47 +0200

    feature: add focusPrevious e2e test (#111)
    Co-authored-by: Le Vivilet <levivlet@pm.me>

---
+++ packages/e2e/src/find-widget-focus-previous.ts
@@ -0,0 +1,32 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'find-widget-focus-previous'
+
+export const skip = 1
+
+export const test: Test = async ({ Command, FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/file1.txt`,
+    `content 1
+content 2`,
+  )
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+  await Editor.setSelections(new Uint32Array([0, 0, 0, 7]))
+  await Editor.openFindWidget()
+  await FindWidget.focusNext()
+  await Command.execute('FindWidget.focusPrevious')
+
+  // act
+  await FindWidget.focusNext()
+
+  // assert
+  const findWidgetInput = Locator('.FindWidget .MultilineInputBox')
+  await expect(findWidgetInput).toBeVisible()
+  await expect(findWidgetInput).toHaveValue('content')
+  const findWidgetMatchCount = Locator(`.FindWidgetMatchCount`)
+  await expect(findWidgetMatchCount).toBeVisible()
+  await expect(findWidgetMatchCount).toHaveText('1 of 2')
+}

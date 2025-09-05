5d40ac72a8cf3a5a1f8317c9ff1ef34b26c9702f
Le Vivilet <levivlet@pm.me>
2025-09-05 11:37:51 +0200

    feature: use minWidth

--- packages/find-widget-worker/src/parts/FindWidgetHandleResizerPointerMove/FindWidgetHandleResizerPointerMove.ts
+++ packages/find-widget-worker/src/parts/FindWidgetHandleResizerPointerMove/FindWidgetHandleResizerPointerMove.ts
@@ -1,12 +1,17 @@
 import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
 
+const getNewWidth = (oldWidth: number, oldX: number, eventX: number, minWidth: number): number => {
+  const oldXEnd = oldX + oldWidth
+  const newWidth = Math.max(oldXEnd - eventX, minWidth)
+  return newWidth
+}
+
 export const handleResizerPointerMove = async (state: FindWidgetState, eventX: number, eventY: number): Promise<FindWidgetState> => {
-  const { width, x, resizerPointerDown } = state
+  const { width, x, resizerPointerDown, minWidth } = state
   if (!resizerPointerDown) {
     return state
   }
-  const oldXEnd = x + width
-  const newWidth = oldXEnd - eventX
+  const newWidth = getNewWidth(width, x, eventX, minWidth)
   return {
     ...state,
     x: eventX,

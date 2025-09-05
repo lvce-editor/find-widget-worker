33d13da781a3f6d7e5ab525068062035f2d6d902
Le Vivilet <levivlet@pm.me>
2025-09-05 09:30:42 +0200

    feature: use i18n string

--- packages/find-widget-worker/src/parts/GetSearchToggleButtonVirtualDom/GetSearchToggleButtonVirtualDom.ts
+++ packages/find-widget-worker/src/parts/GetSearchToggleButtonVirtualDom/GetSearchToggleButtonVirtualDom.ts
@@ -1,15 +1,17 @@
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as FindStrings from '../FindStrings/FindStrings.ts'
 import * as InputName from '../InputName/InputName.ts'
 
 export const getSearchToggleButtonVirtualDom = (replaceExpanded: boolean, onClick = ''): readonly VirtualDomNode[] => {
+  const title = FindStrings.toggleReplace()
   return [
     {
       type: VirtualDomElements.Button,
       className: `IconButton SearchToggleButton ${replaceExpanded ? 'SearchToggleButtonExpanded' : ''}`,
-      title: 'Toggle Replace',
-      ariaLabel: 'Toggle Replace',
+      title,
+      ariaLabel: title,
       ariaExpanded: replaceExpanded,
       name: InputName.ToggleReplace,
       childCount: 1,

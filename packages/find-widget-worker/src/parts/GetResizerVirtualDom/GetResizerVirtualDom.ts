2887fcdf82bc95c6c6a7970353f6090f35bf1c97
Le Vivilet <levivilet@pm.me>
2025-09-05 21:59:16 +0200

    feature: use classnames enum

--- packages/find-widget-worker/src/parts/GetResizerVirtualDom/GetResizerVirtualDom.ts
+++ packages/find-widget-worker/src/parts/GetResizerVirtualDom/GetResizerVirtualDom.ts
@@ -1,19 +1,20 @@
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
 export const getResizerVirtualDom = (): readonly VirtualDomNode[] => {
-  return [
-    {
-      type: VirtualDomElements.Div,
-      className: 'Resizer',
-      childCount: 1,
-      onPointerDown: DomEventListenerFunctions.HandleResizerPointerDown,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: 'ResizerHighlight',
-      childCount: 0,
-    },
-  ]
+	return [
+		{
+			type: VirtualDomElements.Div,
+			className: ClassNames.Resizer,
+			childCount: 1,
+			onPointerDown: DomEventListenerFunctions.HandleResizerPointerDown,
+		},
+		{
+			type: VirtualDomElements.Div,
+			className: ClassNames.ResizerHighlight,
+			childCount: 0,
+		},
+	]
 }
--- packages/find-widget-worker/src/parts/GetSearchToggleButtonVirtualDom/GetSearchToggleButtonVirtualDom.ts
+++ packages/find-widget-worker/src/parts/GetSearchToggleButtonVirtualDom/GetSearchToggleButtonVirtualDom.ts
@@ -1,28 +1,37 @@
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as FindStrings from '../FindStrings/FindStrings.ts'
 import * as InputName from '../InputName/InputName.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 
 export const getSearchToggleButtonVirtualDom = (replaceExpanded: boolean, onClick = ''): readonly VirtualDomNode[] => {
-  const title = FindStrings.toggleReplace()
-  return [
-    {
-      type: VirtualDomElements.Button,
-      className: `IconButton SearchToggleButton ${replaceExpanded ? 'SearchToggleButtonExpanded' : ''}`,
-      title,
-      ariaLabel: title,
-      ariaExpanded: replaceExpanded,
-      name: InputName.ToggleReplace,
-      childCount: 1,
-      'data-command': 'toggleReplace',
-      onClick,
-      onFocus: DomEventListenerFunctions.HandleToggleReplaceFocus,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: `MaskIcon ${replaceExpanded ? 'MaskIconChevronDown' : 'MaskIconChevronRight'}`,
-      childCount: 0,
-    },
-  ]
+	const title = FindStrings.toggleReplace()
+	return [
+		{
+			type: VirtualDomElements.Button,
+			className: MergeClassNames.mergeClassNames(
+				ClassNames.IconButton,
+				ClassNames.SearchToggleButton,
+				replaceExpanded ? ClassNames.SearchToggleButtonExpanded : ''
+			),
+			title,
+			ariaLabel: title,
+			ariaExpanded: replaceExpanded,
+			name: InputName.ToggleReplace,
+			childCount: 1,
+			'data-command': 'toggleReplace',
+			onClick,
+			onFocus: DomEventListenerFunctions.HandleToggleReplaceFocus,
+		},
+		{
+			type: VirtualDomElements.Div,
+			className: MergeClassNames.mergeClassNames(
+				ClassNames.MaskIcon,
+				replaceExpanded ? ClassNames.MaskIconChevronDown : ClassNames.MaskIconChevronRight
+			),
+			childCount: 0,
+		},
+	]
 }

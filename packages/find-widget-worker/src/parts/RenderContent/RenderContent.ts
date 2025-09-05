a700611e5d7fa91278b5ec826d6df86933f7f2f0
Le Vivilet <levivilet@pm.me>
2025-09-05 21:51:16 +0200

    feature: use destructuring (#207)

--- packages/find-widget-worker/src/parts/RenderContent/RenderContent.ts
+++ packages/find-widget-worker/src/parts/RenderContent/RenderContent.ts
@@ -6,20 +6,20 @@ import * as GetMatchCountText from '../GetMatchCountText/GetMatchCountText.ts'
 import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
 
 export const renderContent = (oldState: FindWidgetState, newState: FindWidgetState): readonly any[] => {
-  const { uid } = newState
+  const { uid, matchCount, value, replaceExpanded, matchCase, matchWholeWord, useRegularExpression } = newState
   const matchCountText = GetMatchCountText.getMatchCountText(newState.matchIndex, newState.matchCount)
-  const { findButtonsEnabled, replaceButtonsEnabled } = GetFindWidgetButtonsEnabled.getFindWidgetButtonsEnabled(newState.matchCount, newState.value)
+  const { findButtonsEnabled, replaceButtonsEnabled } = GetFindWidgetButtonsEnabled.getFindWidgetButtonsEnabled(matchCount, value)
   const { findButtons, replaceButtons } = GetFindWidgetButtons.getFindWidgetButtons(findButtonsEnabled, replaceButtonsEnabled)
   const dom = GetFindWidgetVirtualDom.getFindWidgetVirtualDom(
     matchCountText,
-    newState.replaceExpanded,
+    replaceExpanded,
     findButtons,
     replaceButtons,
-    newState.matchCase,
-    newState.matchWholeWord,
-    newState.useRegularExpression,
-    newState.matchCount,
-    newState.value,
+    matchCase,
+    matchWholeWord,
+    useRegularExpression,
+    matchCount,
+    value,
   )
   return [RenderMethod.SetDom2, uid, dom]
 }

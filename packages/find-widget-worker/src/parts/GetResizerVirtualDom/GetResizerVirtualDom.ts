9dc41b66688f810ec1c08f04f8f4cfa390429924
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-05 09:37:08 +0200

    feature: add event listeners to resizer dom (#178)
    Co-authored-by: Le Vivilet <levivlet@pm.me>

--- packages/find-widget-worker/src/parts/GetResizerVirtualDom/GetResizerVirtualDom.ts
+++ packages/find-widget-worker/src/parts/GetResizerVirtualDom/GetResizerVirtualDom.ts
@@ -1,5 +1,6 @@
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
 export const getResizerVirtualDom = (): readonly VirtualDomNode[] => {
   return [
@@ -7,6 +8,9 @@ export const getResizerVirtualDom = (): readonly VirtualDomNode[] => {
       type: VirtualDomElements.Div,
       className: 'Resizer',
       childCount: 1,
+      onPointerDown: DomEventListenerFunctions.HandleResizerPointerDown,
+      onPointerMove: DomEventListenerFunctions.HandleResizerPointerUp,
+      onPointerUp: DomEventListenerFunctions.HandleResizerPointerMove,
     },
     {
       type: VirtualDomElements.Div,

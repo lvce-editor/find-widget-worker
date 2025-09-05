f15ecd7624392ce213d1741c5f7e5be0aa96f20a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-05 09:25:01 +0200

    feature: add diffcss (#169)
    Co-authored-by: Le Vivilet <levivlet@pm.me>

---
+++ packages/find-widget-worker/src/parts/DiffCss/DiffCss.ts
@@ -0,0 +1,5 @@
+import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
+
+export const isEqual = (oldState: FindWidgetState, newState: FindWidgetState): boolean => {
+  return newState.width === newState.width && oldState.height === newState.height && oldState.x === newState.x
+}

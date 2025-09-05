6067fb32ced3aa35024d37d45e800f8d8a9be21e
Le Vivilet <levivlet@pm.me>
2025-09-05 09:29:34 +0200

    feature: add resizer pointer move

---
+++ packages/find-widget-worker/src/parts/FindWidgetHandleResizerPointerMove/FindWidgetHandleResizerPointerMove.ts
@@ -0,0 +1,5 @@
+import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
+
+export const handleResizerPointerMove = async (state: FindWidgetState, eventX: number, eventY: number): Promise<FindWidgetState> => {
+  return state
+}

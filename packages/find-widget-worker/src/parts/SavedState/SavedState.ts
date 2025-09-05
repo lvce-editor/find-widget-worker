bba0431c5fe7016d29b2767ba47b6c6fd14f9487
Le Vivilet <levivilet@pm.me>
2025-09-05 16:01:43 +0200

    feature: save more state for hotreload

--- packages/find-widget-worker/src/parts/SavedState/SavedState.ts
+++ packages/find-widget-worker/src/parts/SavedState/SavedState.ts
@@ -1,3 +1,9 @@
 export interface SavedState {
   readonly width: number
+  readonly value: string
+  readonly replacement: string
+  readonly matchCase: boolean
+  readonly matchWholeWord: boolean
+  readonly useRegularExpression: boolean
+  readonly replaceExpanded: boolean
 }

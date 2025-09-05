a20f30482d4658721d1665279f268d6c637937e1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-05 09:50:53 +0200

    feature: on pointer move, update width (#182)
    Co-authored-by: Le Vivilet <levivlet@pm.me>

--- packages/find-widget-worker/src/parts/FindWidgetHandleResizerPointerMove/FindWidgetHandleResizerPointerMove.ts
+++ packages/find-widget-worker/src/parts/FindWidgetHandleResizerPointerMove/FindWidgetHandleResizerPointerMove.ts
@@ -1,5 +1,12 @@
 import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
 
 export const handleResizerPointerMove = async (state: FindWidgetState, eventX: number, eventY: number): Promise<FindWidgetState> => {
-  return state
+  const { width, x } = state
+  const oldXEnd = x + width
+  const newWidth = oldXEnd - eventX
+  return {
+    ...state,
+    x: eventX,
+    width: newWidth,
+  }
 }

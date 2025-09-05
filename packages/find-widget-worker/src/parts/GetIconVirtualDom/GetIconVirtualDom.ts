778cd0d8bb580f23cb1856743690aea0970abd7e
Le Vivilet <levivilet@pm.me>
2025-09-05 22:17:11 +0200

    feature: use classnames enum (#214)
    * feature: use classnames enum
    
    * test
    
    * lint

--- packages/find-widget-worker/src/parts/GetIconVirtualDom/GetIconVirtualDom.ts
+++ packages/find-widget-worker/src/parts/GetIconVirtualDom/GetIconVirtualDom.ts
@@ -1,10 +1,11 @@
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import { AriaRoles } from '@lvce-editor/virtual-dom-worker'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
 
 export const getIconVirtualDom = (icon: string, type = VirtualDomElements.Div) => {
   return {
     type,
-    className: `MaskIcon MaskIcon${icon}`,
+    className: `${ClassNames.MaskIcon} MaskIcon${icon}`,
     role: AriaRoles.None,
     childCount: 0,
   }

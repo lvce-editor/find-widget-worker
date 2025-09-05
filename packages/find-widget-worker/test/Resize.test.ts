09ad92f5bc24a81e2a36b1b728df3860dda70cf0
Le Vivilet <levivilet@pm.me>
2025-09-05 23:27:26 +0200

    feature: add more tests

---
+++ packages/find-widget-worker/test/Resize.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from '@jest/globals'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as Resize from '../src/parts/Resize/Resize.ts'
+
+test('resize - returns same state (no-op)', async () => {
+	const state = { ...createDefaultState(), width: 100, height: 20, x: 10, y: 10 }
+	const dimensions = { x: 20, y: 30, width: 200, height: 40 }
+	const result = await Resize.resize(state, dimensions)
+	expect(result).toBe(state)
+})
---
+++ packages/find-widget-worker/test/SaveState.test.ts
@@ -0,0 +1,27 @@
+import { expect, test } from '@jest/globals'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as SaveState from '../src/parts/SaveState/SaveState.ts'
+
+test('saveState - picks subset of fields', () => {
+	const base = createDefaultState()
+	const state = {
+		...base,
+		width: 512,
+		value: 'abc',
+		replacement: 'x',
+		matchCase: true,
+		matchWholeWord: true,
+		useRegularExpression: true,
+		replaceExpanded: true,
+	}
+	const saved = SaveState.saveState(state)
+	expect(saved).toEqual({
+		width: 512,
+		value: 'abc',
+		replacement: 'x',
+		matchCase: true,
+		matchWholeWord: true,
+		useRegularExpression: true,
+		replaceExpanded: true,
+	})
+})

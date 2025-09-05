72ea31050495bf90ea74fd92ee9e6de2190c7220
Le Vivilet <levivilet@pm.me>
2025-09-05 22:08:19 +0200

    feature: move find buttons to findWidgetButtons

--- packages/find-widget-worker/src/parts/FindWidgetButtons/FindWidgetButtons.ts
+++ packages/find-widget-worker/src/parts/FindWidgetButtons/FindWidgetButtons.ts
@@ -1,6 +1,8 @@
 import type { FindWidgetButton } from '../FindWidgetButton/FindWidgetButton.ts'
+import type { ISearchFieldButton } from '../ISearchFieldButton/ISearchFieldButton.ts'
 
 export interface FindWidgetButtons {
   readonly findButtons: readonly FindWidgetButton[]
   readonly replaceButtons: readonly FindWidgetButton[]
+  readonly findFieldButtons: readonly ISearchFieldButton[]
 }
--- packages/find-widget-worker/src/parts/GetFindWidgedButtons/GetFindWidgetButtons.ts
+++ packages/find-widget-worker/src/parts/GetFindWidgedButtons/GetFindWidgetButtons.ts
@@ -1,11 +1,36 @@
 import type { FindWidgetButton } from '../FindWidgetButton/FindWidgetButton.ts'
 import type { FindWidgetButtons } from '../FindWidgetButtons/FindWidgetButtons.ts'
+import type { ISearchFieldButton } from '../ISearchFieldButton/ISearchFieldButton.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as FindStrings from '../FindStrings/FindStrings.ts'
 import * as Icon from '../Icon/Icon.ts'
+import * as InputName from '../InputName/InputName.ts'
 import * as Names from '../InputName/InputName.ts'
 
 export const getFindWidgetButtons = (findButtonsEnabled: boolean, replaceButtonsEnabled: boolean): FindWidgetButtons => {
+  const findFieldButtons: readonly ISearchFieldButton[] = [
+    {
+      checked: false,
+      icon: Icon.MatchCase,
+      title: FindStrings.matchCase(),
+      name: InputName.MatchCase,
+      onClick: DomEventListenerFunctions.HandleClickButton,
+    },
+    {
+      checked: false,
+      icon: Icon.MatchWholeWord,
+      title: FindStrings.matchWholeWord(),
+      name: InputName.MatchWholeWord,
+      onClick: DomEventListenerFunctions.HandleClickButton,
+    },
+    {
+      checked: false,
+      icon: Icon.UseRegularExpression,
+      title: FindStrings.useRegularExpression(),
+      name: InputName.UseRegularExpression,
+      onClick: DomEventListenerFunctions.HandleClickButton,
+    },
+  ]
   const findButtons: readonly FindWidgetButton[] = [
     {
       label: FindStrings.previousMatch(),
@@ -46,6 +71,7 @@ export const getFindWidgetButtons = (findButtonsEnabled: boolean, replaceButtons
     },
   ]
   return {
+    findFieldButtons,
     findButtons,
     replaceButtons,
   }

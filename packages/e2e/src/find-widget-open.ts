a8799acfe442ee54cda132cd16b575b0fb916353
Le Vivilet <levivilet@pm.me>
2025-09-05 21:36:07 +0200

    feature: enable open e2e test

--- packages/e2e/src/find-widget-open.ts
+++ packages/e2e/src/find-widget-open.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'find-widget-open'
 
-export const skip = 1
-
 export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()

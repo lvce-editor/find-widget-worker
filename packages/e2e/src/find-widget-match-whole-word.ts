9f6f33adf4264d345470720aedaacb6207d98b41
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-05 09:14:34 +0200

    feature: add matchWholeWord e2e test (#166)
    Co-authored-by: Le Vivilet <levivlet@pm.me>

---
+++ packages/e2e/src/find-widget-match-whole-word.ts
@@ -0,0 +1,28 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'find-widget-match-whole-word'
+
+export const skip = 1
+
+export const test: Test = async ({ Command, FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/file1.txt`,
+    `content 1
+content 2`,
+  )
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+  await Editor.setSelections(new Uint32Array([0, 0, 0, 7]))
+  await Editor.openFindWidget()
+  await FindWidget.setValue(`Content`)
+  await Command.execute(`FindWidget.toggleMatchWholeWord`)
+
+  // assert
+  const matchCaseCheckBox = Locator(`.SearchFieldButton[name="MatchWholeWord"]`)
+  await expect(matchCaseCheckBox).toHaveAttribute(`aria-checked`, 'true')
+  const findWidgetMatchCount = Locator(`.FindWidgetMatchCount`)
+  await expect(findWidgetMatchCount).toBeVisible()
+  await expect(findWidgetMatchCount).toHaveText('No Results')
+}

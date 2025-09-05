be5d019bc9ac6158304e733954c8e5c754d58482
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-05 11:25:59 +0200

    feature: ignore events if pointer is not down (#188)
    Co-authored-by: Le Vivilet <levivlet@pm.me>

--- packages/find-widget-worker/src/parts/FindWidgetHandleResizerPointerMove/FindWidgetHandleResizerPointerMove.ts
+++ packages/find-widget-worker/src/parts/FindWidgetHandleResizerPointerMove/FindWidgetHandleResizerPointerMove.ts
@@ -1,7 +1,10 @@
 import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
 
 export const handleResizerPointerMove = async (state: FindWidgetState, eventX: number, eventY: number): Promise<FindWidgetState> => {
-  const { width, x } = state
+  const { width, x, resizerPointerDown } = state
+  if (!resizerPointerDown) {
+    return state
+  }
   const oldXEnd = x + width
   const newWidth = oldXEnd - eventX
   return {

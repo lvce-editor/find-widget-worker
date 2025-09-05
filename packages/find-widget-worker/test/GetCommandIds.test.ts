db896c860e643b66c555c8d4091da02223105f8f
Le Vivilet <levivilet@pm.me>
2025-09-05 23:40:14 +0200

    feature: remove unused code (#236)

--- packages/find-widget-worker/test/GetCommandIds.test.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@jest/globals'
-import { getCommandIds } from '../src/parts/GetCommandIds/GetCommandIds.ts'
-
-test('getCommandIds returns all command IDs', () => {
-  const commandIds = getCommandIds()
-  expect(commandIds).toBeDefined()
-})

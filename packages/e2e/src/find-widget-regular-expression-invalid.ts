82602820b5bd252cce20093ccbb753c3cfedf925
Le Vivilet <levivilet@pm.me>
2025-09-06 15:58:23 +0200

    feature: add invalid regex e2e test (#272)

---
+++ packages/e2e/src/find-widget-regular-expression-invalid.ts
@@ -0,0 +1,30 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'find-widget-regular-expression-invalid'
+
+export const skip = 1
+
+export const test: Test = async ({ Command, FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/file1.txt`,
+    `content 1
+content 2`,
+  )
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+  await Editor.setSelections(new Uint32Array([0, 0, 0, 0]))
+  await Editor.openFindWidget()
+  await FindWidget.toggleUseRegularExpression()
+
+  // act
+  await FindWidget.setValue(`(`)
+
+  // assert
+  const matchCaseCheckBox = Locator(`.SearchFieldButton[name="UseRegularExpression"]`)
+  await expect(matchCaseCheckBox).toHaveAttribute(`aria-checked`, 'true')
+  const findWidgetMatchCount = Locator(`.FindWidgetMatchCount`)
+  await expect(findWidgetMatchCount).toBeVisible()
+  await expect(findWidgetMatchCount).toHaveText('No Results')
+}

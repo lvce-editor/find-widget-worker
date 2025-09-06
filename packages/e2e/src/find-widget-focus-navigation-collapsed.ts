ca416d029efd37ea7748fc239b4a4dc5eb788345
Le Vivilet <levivilet@pm.me>
2025-09-06 10:34:54 +0200

    feature: add focus navigation e2e test

---
+++ packages/e2e/src/find-widget-focus-navigation-collapsed.ts
@@ -0,0 +1,28 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'find-widget-focus-navigation-collapsed'
+
+export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, ``)
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+  await Editor.setSelections(new Uint32Array([0, 0, 0, 0]))
+
+  // act
+  await Editor.openFindWidget()
+
+  // assert
+  const findWidgetInput = Locator('.FindWidget .MultilineInputBox')
+  await expect(findWidgetInput).toBeVisible()
+  await expect(findWidgetInput).toHaveValue('')
+  await expect(findWidgetInput).toBeFocused()
+
+  // act
+  await FindWidget.focusNext()
+
+  // assert
+
+  // TODO
+}

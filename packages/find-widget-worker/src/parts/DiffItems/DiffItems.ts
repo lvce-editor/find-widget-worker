71c3f5efd50dd29659f16d7e9011faff9a20361f
Le Vivilet <levivilet@pm.me>
2025-09-06 12:33:49 +0200

    feature: rerender when buttons change (#261)

--- packages/find-widget-worker/src/parts/DiffItems/DiffItems.ts
+++ packages/find-widget-worker/src/parts/DiffItems/DiffItems.ts
@@ -5,6 +5,10 @@ export const isEqual = (oldState: FindWidgetState, newState: FindWidgetState): b
     oldState.version === newState.version &&
     oldState.replaceExpanded === newState.replaceExpanded &&
     oldState.matchCount === newState.matchCount &&
-    oldState.matchIndex === newState.matchIndex
+    oldState.matchIndex === newState.matchIndex &&
+    oldState.matchCase === newState.matchCase &&
+    oldState.matchWholeWord === newState.matchWholeWord &&
+    oldState.useRegularExpression === newState.useRegularExpression &&
+    oldState.preserveCase === newState.preserveCase
   )
 }

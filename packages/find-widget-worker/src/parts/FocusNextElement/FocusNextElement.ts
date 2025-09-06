b69b2289c6c30de1bc1611d146fb6e9d60b18817
Le Vivilet <levivilet@pm.me>
2025-09-06 12:00:04 +0200

    feature: implement focusNextElement (#246)

--- packages/find-widget-worker/src/parts/FocusNextElement/FocusNextElement.ts
+++ packages/find-widget-worker/src/parts/FocusNextElement/FocusNextElement.ts
@@ -1,6 +1,13 @@
+import { InputSource } from '@lvce-editor/constants'
 import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
+import { getNextFocus } from '../GetNextFocus/GetNextFocus.ts'
 
 export const focusNextElement = (state: FindWidgetState): FindWidgetState => {
-  // TODO
-  return state
+  const { focus, replaceExpanded } = state
+  const nextFocus = getNextFocus(focus, replaceExpanded)
+  return {
+    ...state,
+    focus: nextFocus,
+    focusSource: InputSource.Script,
+  }
 }
--- packages/find-widget-worker/src/parts/FocusPreviousElement/FocusPreviousElement.ts
+++ packages/find-widget-worker/src/parts/FocusPreviousElement/FocusPreviousElement.ts
@@ -1,6 +1,13 @@
+import { InputSource } from '@lvce-editor/constants'
 import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
+import { getPreviousFocus } from '../GetPreviousFocus/GetPreviousFocus.ts'
 
 export const focusPreviousElement = (state: FindWidgetState): FindWidgetState => {
-  // TODO
-  return state
+  const { focus, replaceExpanded } = state
+  const previousFocus = getPreviousFocus(focus, replaceExpanded)
+  return {
+    ...state,
+    focus: previousFocus,
+    focusSource: InputSource.Script,
+  }
 }

cb03f23d566ba85f1c8f6c0bb249cf178c08272a
Le Vivilet <levivilet@pm.me>
2025-09-06 12:43:57 +0200

    feature: disable flaky e2e test (#263)

--- packages/e2e/src/find-widget-accessibility.ts
+++ packages/e2e/src/find-widget-accessibility.ts
@@ -33,6 +33,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.editor-find-widget-accessibility'
 
+export const skip = 1
+
 export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()

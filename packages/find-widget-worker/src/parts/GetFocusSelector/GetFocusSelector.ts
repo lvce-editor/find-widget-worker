e96bb0dc1d6001d0d91d6a9348b7c6dd4f4f9dcd
Le Vivilet <levivilet@pm.me>
2025-09-06 12:04:54 +0200

    fix

--- packages/find-widget-worker/src/parts/GetFocusSelector/GetFocusSelector.ts
+++ packages/find-widget-worker/src/parts/GetFocusSelector/GetFocusSelector.ts
@@ -7,26 +7,16 @@ export const getFocusSelector = (focusKey: number): string => {
       return InputName.SearchValue
     case WhenExpression.FocusSearchReplaceInput:
       return InputName.ReplaceValue
-    case WhenExpression.FocusToggleDetails:
-      return InputName.ToggleSearchDetails
     case WhenExpression.FocusSearchMatchCase:
       return InputName.MatchCase
     case WhenExpression.FocusSearchPreserveCase:
       return InputName.PreserveCase
     case WhenExpression.FocusSearchRegex:
       return InputName.UseRegularExpression
-    case WhenExpression.FocusSearchIncludeInput:
-      return InputName.FilesToInclude
-    case WhenExpression.FocusSearchExcludeInput:
-      return InputName.FilesToExclude
     case WhenExpression.FocusSearchWholeWord:
       return InputName.MatchWholeWord
     case WhenExpression.FocusSearchReplaceAll:
       return InputName.ReplaceAll
-    case WhenExpression.FocusSearchOpenEditors:
-      return InputName.SearchOnlyOpenEditors
-    case WhenExpression.FocusIgnoreFiles:
-      return InputName.UseExcludeSettings
     case WhenExpression.FocusToggleReplace:
       return InputName.ToggleReplace
     default:

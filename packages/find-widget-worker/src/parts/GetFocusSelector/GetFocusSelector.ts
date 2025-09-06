fd685d1e0855170a88e8edbfbda491a176a7c312
Le Vivilet <levivilet@pm.me>
2025-09-06 12:06:50 +0200

    feature: add getFocusSelector (#250)
    * feature: add getFocusSelector
    
    * fix
    
    * test

---
+++ packages/find-widget-worker/src/parts/GetFocusSelector/GetFocusSelector.ts
@@ -0,0 +1,25 @@
+import { WhenExpression } from '@lvce-editor/constants'
+import * as InputName from '../InputName/InputName.ts'
+
+export const getFocusSelector = (focusKey: number): string => {
+  switch (focusKey) {
+    case WhenExpression.FocusSearchInput:
+      return InputName.SearchValue
+    case WhenExpression.FocusSearchReplaceInput:
+      return InputName.ReplaceValue
+    case WhenExpression.FocusSearchMatchCase:
+      return InputName.MatchCase
+    case WhenExpression.FocusSearchPreserveCase:
+      return InputName.PreserveCase
+    case WhenExpression.FocusSearchRegex:
+      return InputName.UseRegularExpression
+    case WhenExpression.FocusSearchWholeWord:
+      return InputName.MatchWholeWord
+    case WhenExpression.FocusSearchReplaceAll:
+      return InputName.ReplaceAll
+    case WhenExpression.FocusToggleReplace:
+      return InputName.ToggleReplace
+    default:
+      return ''
+  }
+}
---
+++ packages/find-widget-worker/test/GetFocusSelector.test.ts
@@ -0,0 +1,36 @@
+import { expect, test } from '@jest/globals'
+import { WhenExpression } from '@lvce-editor/constants'
+import * as GetFocusSelector from '../src/parts/GetFocusSelector/GetFocusSelector.ts'
+import * as InputName from '../src/parts/InputName/InputName.ts'
+
+test('getFocusSelector - search input', () => {
+  expect(GetFocusSelector.getFocusSelector(WhenExpression.FocusSearchInput)).toBe(InputName.SearchValue)
+})
+
+test('getFocusSelector - replace input', () => {
+  expect(GetFocusSelector.getFocusSelector(WhenExpression.FocusSearchReplaceInput)).toBe(InputName.ReplaceValue)
+})
+
+test('getFocusSelector - match case', () => {
+  expect(GetFocusSelector.getFocusSelector(WhenExpression.FocusSearchMatchCase)).toBe('MatchCase')
+})
+
+test('getFocusSelector - preserve case', () => {
+  expect(GetFocusSelector.getFocusSelector(WhenExpression.FocusSearchPreserveCase)).toBe('PreserveCase')
+})
+
+test('getFocusSelector - regex', () => {
+  expect(GetFocusSelector.getFocusSelector(WhenExpression.FocusSearchRegex)).toBe('UseRegularExpression')
+})
+
+test('getFocusSelector - match whole word', () => {
+  expect(GetFocusSelector.getFocusSelector(WhenExpression.FocusSearchWholeWord)).toBe('MatchWholeWord')
+})
+
+test('getFocusSelector - unknown focus key', () => {
+  expect(GetFocusSelector.getFocusSelector(-1)).toBe('')
+})
+
+test('getFocusSelector - replace all', () => {
+  expect(GetFocusSelector.getFocusSelector(WhenExpression.FocusSearchReplaceAll)).toBe('ReplaceAll')
+})

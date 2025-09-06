cf72deef6f617cf59d7251dc8443f161a5bfd4b9
Le Vivilet <levivilet@pm.me>
2025-09-06 12:01:01 +0200

    feature: use inputName enum (#247)

--- packages/find-widget-worker/src/parts/RenderFocus/RenderFocus.ts
+++ packages/find-widget-worker/src/parts/RenderFocus/RenderFocus.ts
@@ -1,7 +1,8 @@
 import { ViewletCommand } from '@lvce-editor/constants'
 import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
+import * as InputName from '../InputName/InputName.ts'
 
 export const renderFocus = (oldState: FindWidgetState, newState: FindWidgetState): readonly any[] => {
   const { uid } = newState
-  return [/* method */ ViewletCommand.FocusSelector, uid, `[name="search-value"]`]
+  return [ViewletCommand.FocusSelector, uid, `[name="${InputName.SearchValue}"]`]
 }

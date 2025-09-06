c5b4a50806df265a63ac75dc6a7742021bee4b7d
Le Vivilet <levivilet@pm.me>
2025-09-06 12:04:15 +0200

    feature: add getFocusSelector

---
+++ packages/find-widget-worker/src/parts/GetFocusSelector/GetFocusSelector.ts
@@ -0,0 +1,35 @@
+import { WhenExpression } from '@lvce-editor/constants'
+import * as InputName from '../InputName/InputName.ts'
+
+export const getFocusSelector = (focusKey: number): string => {
+  switch (focusKey) {
+    case WhenExpression.FocusSearchInput:
+      return InputName.SearchValue
+    case WhenExpression.FocusSearchReplaceInput:
+      return InputName.ReplaceValue
+    case WhenExpression.FocusToggleDetails:
+      return InputName.ToggleSearchDetails
+    case WhenExpression.FocusSearchMatchCase:
+      return InputName.MatchCase
+    case WhenExpression.FocusSearchPreserveCase:
+      return InputName.PreserveCase
+    case WhenExpression.FocusSearchRegex:
+      return InputName.UseRegularExpression
+    case WhenExpression.FocusSearchIncludeInput:
+      return InputName.FilesToInclude
+    case WhenExpression.FocusSearchExcludeInput:
+      return InputName.FilesToExclude
+    case WhenExpression.FocusSearchWholeWord:
+      return InputName.MatchWholeWord
+    case WhenExpression.FocusSearchReplaceAll:
+      return InputName.ReplaceAll
+    case WhenExpression.FocusSearchOpenEditors:
+      return InputName.SearchOnlyOpenEditors
+    case WhenExpression.FocusIgnoreFiles:
+      return InputName.UseExcludeSettings
+    case WhenExpression.FocusToggleReplace:
+      return InputName.ToggleReplace
+    default:
+      return ''
+  }
+}

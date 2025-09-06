1f6aa4081e1186365cd5343954cc08deb155828c
Le Vivilet <levivilet@pm.me>
2025-09-06 15:57:47 +0200

    feature: add focus e2e test

---
+++ packages/e2e/src/find-widget-focus-navigation-collapsed-no-results-backward.ts
@@ -0,0 +1,51 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'find-widget-focus-navigation-collapsed-no-results-backward'
+
+export const skip = 1
+
+export const test: Test = async ({ Command, FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, ``)
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+  await Editor.setSelections(new Uint32Array([0, 0, 0, 0]))
+
+  // act
+  await Editor.openFindWidget()
+
+  // assert
+  const findWidgetInput = Locator('.FindWidget .MultilineInputBox')
+  await expect(findWidgetInput).toBeVisible()
+  await expect(findWidgetInput).toHaveValue('')
+  await expect(findWidgetInput).toBeFocused()
+
+  // act
+  await Command.execute(`FindWidget.focusNextElement`)
+
+  // assert
+  const matchCaseButton = Locator(`.SearchFieldButton[name="MatchCase"]`)
+  await expect(matchCaseButton).toBeFocused()
+
+  // act
+  await Command.execute(`FindWidget.focusNextElement`)
+
+  // assert
+  const matchWholeWordButton = Locator(`.SearchFieldButton[name="MatchWholeWord"]`)
+  await expect(matchWholeWordButton).toBeFocused()
+
+  // act
+  await Command.execute(`FindWidget.focusNextElement`)
+
+  // assert
+  const useRegularExpressionButton = Locator(`.SearchFieldButton[name="UseRegularExpression"]`)
+  await expect(useRegularExpressionButton).toBeFocused()
+
+  // act
+  await Command.execute(`FindWidget.focusNextElement`)
+
+  // assert
+  const closeButton = Locator(`.IconButton[name="Close"]`)
+  await expect(closeButton).toBeFocused()
+}

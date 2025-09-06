4142d8f1322d4cda18fae7b3496a589a59d5c7ea
Le Vivilet <levivilet@pm.me>
2025-09-06 12:09:33 +0200

    feature: update diff (#251)
    * feature: update diff
    
    * diff

--- packages/find-widget-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/find-widget-worker/src/parts/DiffModules/DiffModules.ts
@@ -1,6 +1,7 @@
 import * as DiffBounds from '../DiffBounds/DiffBounds.ts'
 import * as DiffCss from '../DiffCss/DiffCss.ts'
 import * as DiffEventListeners from '../DiffEventListeners/DiffEventListeners.ts'
+import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffFocusContext from '../DiffFocusContext/DiffFocusContext.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffParentUid from '../DiffParentUid/DiffParentUid.ts'
@@ -13,7 +14,7 @@ export const modules = [
   DiffBounds.isEqual,
   DiffParentUid.isEqual,
   DiffFocusContext.isEqual,
-  DiffFocusContext.isEqual,
+  DiffFocus.isEqual,
   DiffValue.isEqual,
   DiffCss.isEqual,
 ]

fc3dd709dc9102f851106d0aa1ef10540dd22c19
Le Vivilet <levivilet@pm.me>
2025-09-10 12:45:21 +0200

    feature: hoist classname (#288)

--- packages/find-widget-worker/src/parts/GetFindMatchCountClassName/GetFindMatchCountClassName.ts
+++ packages/find-widget-worker/src/parts/GetFindMatchCountClassName/GetFindMatchCountClassName.ts
@@ -1,9 +1,11 @@
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 
+const emptyMatches = MergeClassNames.mergeClassNames(ClassNames.FindWidgetMatchCount, ClassNames.FindWidgetMatchCountEmpty)
+
 export const getFindMatchCountClassName = (matchCount: number, hasValue: boolean): string => {
   if (hasValue && matchCount === 0) {
-    return MergeClassNames.mergeClassNames(ClassNames.FindWidgetMatchCount, ClassNames.FindWidgetMatchCountEmpty)
+    return emptyMatches
   }
   return ClassNames.FindWidgetMatchCount
 }

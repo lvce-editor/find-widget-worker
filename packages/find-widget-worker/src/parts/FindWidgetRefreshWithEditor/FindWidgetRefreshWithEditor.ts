4a397ddb21f59c2dbc04e275db98dd78bc2788d2
Le Vivilet <levivilet@pm.me>
2025-09-10 09:42:14 +0200

    feature: update height on refresh

--- packages/find-widget-worker/src/parts/FindWidgetRefreshWithEditor/FindWidgetRefreshWithEditor.ts
+++ packages/find-widget-worker/src/parts/FindWidgetRefreshWithEditor/FindWidgetRefreshWithEditor.ts
@@ -1,25 +1,29 @@
 import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
 import * as FindMatches from '../FindMatches/FindMatches.ts'
 import * as GetMatchCount from '../GetMatchCount/GetMatchCount.ts'
+import { measureTextHeight } from '../MeasureTextHeight/MeasureTextHeight.ts'
 
 export const refresh = (state: FindWidgetState, value: string, inputSource: number): FindWidgetState => {
-  const { lines, matchCase, useRegularExpression, matchWholeWord } = state
+  const { lines, matchCase, useRegularExpression, matchWholeWord, inputLineHeight } = state
   const options: FindMatches.FindOptions = {
     matchCase,
     useRegularExpression,
     matchWholeWord,
   }
   const { matches, error } = FindMatches.findMatches(lines, value, options)
+  const inputHeight = measureTextHeight(value, inputLineHeight)
   if (error) {
     return {
       ...state,
       inputErrorMessage: error,
+      inputHeight,
     }
   }
   const matchCount = GetMatchCount.getMatchCount(matches)
   return {
     ...state,
     inputErrorMessage: error,
+    inputHeight,
     inputSource,
     matchCount,
     matches,

a805917bec072a7b704a5611d2743744b8ac0f14
Le Vivilet <levivilet@pm.me>
2025-09-10 09:38:38 +0200

    update css

--- packages/find-widget-worker/src/parts/RenderCss/RenderCss.ts
+++ packages/find-widget-worker/src/parts/RenderCss/RenderCss.ts
@@ -4,11 +4,11 @@ import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
 export const renderCss = (oldState: FindWidgetState, newState: FindWidgetState): readonly any[] => {
   const { uid, width, height, x, y, inputHeight } = newState
   const css = `:root {
-    --FindWidgetWidth: ${width}px;
-    --FindWidgetHeight: ${height}px;
-    --FindWidgetX: ${x}px;
-    --FindWidgetY: ${y}px;
-    --FindWidgetInputHeight: ${inputHeight}px;
-  }`
+  --FindWidgetWidth: ${width}px;
+  --FindWidgetHeight: ${height}px;
+  --FindWidgetX: ${x}px;
+  --FindWidgetY: ${y}px;
+  --FindWidgetInputHeight: ${inputHeight}px;
+}`
   return [ViewletCommand.SetCss, uid, css]
 }

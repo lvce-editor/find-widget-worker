033cf15e0d635795d8c25fe12d4d6b93257d34a7
Le Vivilet <levivilet@pm.me>
2025-09-10 09:35:23 +0200

    feature: diff css input height (#282)

--- packages/find-widget-worker/src/parts/DiffCss/DiffCss.ts
+++ packages/find-widget-worker/src/parts/DiffCss/DiffCss.ts
@@ -1,5 +1,10 @@
 import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
 
 export const isEqual = (oldState: FindWidgetState, newState: FindWidgetState): boolean => {
-  return newState.width === newState.width && oldState.height === newState.height && oldState.x === newState.x
+  return (
+    newState.width === newState.width &&
+    oldState.height === newState.height &&
+    oldState.x === newState.x &&
+    oldState.inputHeight === newState.inputHeight
+  )
 }

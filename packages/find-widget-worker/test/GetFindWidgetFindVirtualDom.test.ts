cd6c31226d92222513c2171089dc3cbf4c5f3420
Le Vivilet <levivilet@pm.me>
2025-09-10 13:00:35 +0200

    feature: add actions wrapper element to allow for dynamic input height (#291)

--- packages/find-widget-worker/test/GetFindWidgetFindVirtualDom.test.ts
+++ packages/find-widget-worker/test/GetFindWidgetFindVirtualDom.test.ts
@@ -44,7 +44,7 @@ test('getFindWidgetFindVirtualDom returns correct virtual dom elements with no m
     {
       type: VirtualDomElements.Div,
       className: ClassNames.FindWidgetFind,
-      childCount: 5,
+      childCount: 2,
     },
     {
       type: VirtualDomElements.Div,
@@ -116,6 +116,11 @@ test('getFindWidgetFindVirtualDom returns correct virtual dom elements with no m
       type: VirtualDomElements.Div,
       className: 'MaskIcon MaskIconRegex',
       childCount: 0,
+    },
+    {
+      childCount: 1,
+      className: 'FindWidgetActions',
+      type: 4,
     },
     {
       type: VirtualDomElements.Div,
@@ -174,7 +179,7 @@ test('getFindWidgetFindVirtualDom returns correct virtual dom elements with matc
     {
       type: VirtualDomElements.Div,
       className: ClassNames.FindWidgetFind,
-      childCount: 5,
+      childCount: 2,
     },
     {
       type: VirtualDomElements.Div,
@@ -246,6 +251,11 @@ test('getFindWidgetFindVirtualDom returns correct virtual dom elements with matc
       type: VirtualDomElements.Div,
       className: 'MaskIcon MaskIconRegex',
       childCount: 0,
+    },
+    {
+      childCount: 2,
+      className: 'FindWidgetActions',
+      type: 4,
     },
     {
       type: VirtualDomElements.Div,

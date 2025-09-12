630dd840311635246dbf89912a6b5f605bea85fd
Le Vivilet <levivilet@pm.me>
2025-09-12 16:52:47 +0200

    feature: measure height in loadContent

--- packages/find-widget-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/find-widget-worker/src/parts/LoadContent/LoadContent.ts
@@ -7,34 +7,35 @@ import * as GetLines from '../GetLines/GetLines.ts'
 import * as GetMatchCount from '../GetMatchCount/GetMatchCount.ts'
 import * as GetSelectedText from '../GetSelectedText/GetSelectedText.ts'
 import * as GetSelections from '../GetSelections/GetSelections.ts'
+import { measureInputHeight } from '../MeasureInputHeight/MeasureInputHeight.ts'
 import { measureTextHeight } from '../MeasureTextHeight/MeasureTextHeight.ts'
 import { restoreState } from '../RestoreState/RestoreState.ts'
 
 export const loadContent = async (state: FindWidgetState, savedState?: any): Promise<FindWidgetState> => {
-  const { editorUid, editorWidth, editorX, editorY, inputLineHeight } = state
+  const { editorUid, editorWidth, editorX, editorY, inputLineHeight, inputPaddingBottom, inputPaddingTop, replaceExpanded } = state
   const { replacement, value } = restoreState(savedState)
   const lines = await GetLines.getLines(editorUid)
   const selections = await GetSelections.getSelections(editorUid)
-  const { x, y, width, height } = GetFindWidgetPosition.getFindWidgetPosition(editorX, editorY, editorWidth)
+  const { x, y, width } = GetFindWidgetPosition.getFindWidgetPosition(editorX, editorY, editorWidth)
   if (lines.length === 0) {
     return state
   }
   const actualValue = value || GetSelectedText.getSelectedText(lines, selections)
   const matches = FindMatchesCaseInsensitive.findMatchesCaseInsensitive(lines, actualValue)
   const matchCount = GetMatchCount.getMatchCount(matches)
-  const inputHeight = measureTextHeight(value, inputLineHeight)
+  const { inputHeight, height } = measureInputHeight(value, inputLineHeight, inputPaddingBottom, inputPaddingTop, replaceExpanded)
   return {
     ...state,
     focus: WhenExpression.FocusSearchInput,
     focused: true,
     height,
-    searchInputHeight: inputHeight,
     inputSource: InputSource.Script,
     lines,
     matchCount,
     matches,
     matchIndex: 0,
     replacement,
+    searchInputHeight: inputHeight,
     selections,
     value: actualValue,
     version: 1,

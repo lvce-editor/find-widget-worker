10398b2865e5a97c577ff8fd9acb55a01769e8b8
Le Vivilet <levivilet@pm.me>
2025-09-12 16:49:05 +0200

    feature: measure both height and inputHeight

--- packages/find-widget-worker/src/parts/FindWidgetRefreshWithEditor/FindWidgetRefreshWithEditor.ts
+++ packages/find-widget-worker/src/parts/FindWidgetRefreshWithEditor/FindWidgetRefreshWithEditor.ts
@@ -4,15 +4,14 @@ import * as GetMatchCount from '../GetMatchCount/GetMatchCount.ts'
 import { measureInputHeight } from '../MeasureInputHeight/MeasureInputHeight.ts'
 
 export const refresh = (state: FindWidgetState, value: string, inputSource: number): FindWidgetState => {
-  const { lines, matchCase, useRegularExpression, matchWholeWord, inputLineHeight, inputPaddingTop, inputPaddingBottom } = state
+  const { lines, matchCase, useRegularExpression, matchWholeWord, inputLineHeight, inputPaddingTop, inputPaddingBottom, replaceExpanded } = state
   const options: FindMatches.FindOptions = {
     matchCase,
     useRegularExpression,
     matchWholeWord,
   }
   const { matches, error } = FindMatches.findMatches(lines, value, options)
-  const inputHeight = measureInputHeight(value, inputLineHeight, inputPaddingBottom, inputPaddingTop)
-  const height = inputHeight + 5
+  const { inputHeight, height } = measureInputHeight(value, inputLineHeight, inputPaddingBottom, inputPaddingTop, replaceExpanded)
   if (error) {
     return {
       ...state,

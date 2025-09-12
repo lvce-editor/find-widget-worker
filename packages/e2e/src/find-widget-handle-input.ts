c19514ec12de961910a94d73285d417f28469815
Le Vivilet <levivilet@pm.me>
2025-09-12 16:32:18 +0200

    feature: add input e2e test

---
+++ packages/e2e/src/find-widget-handle-input.ts
@@ -0,0 +1,25 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'find-widget-handle-input'
+
+export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/file1.txt`,
+    `content 1
+content 2`,
+  )
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+  await Editor.setSelections(new Uint32Array([0, 0, 0, 0]))
+  await Editor.openFindWidget()
+
+  // act
+  await FindWidget.setValue('abc')
+
+  // assert
+  const findWidgetInput = Locator('.FindWidget .MultilineInputBox')
+  await expect(findWidgetInput).toBeVisible()
+  await expect(findWidgetInput).toHaveValue('abc')
+}

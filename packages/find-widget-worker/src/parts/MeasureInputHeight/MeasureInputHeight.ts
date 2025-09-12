337e8891d4f8dd11e1ba2de4b1af84a72605ddae
Le Vivilet <levivilet@pm.me>
2025-09-12 16:50:20 +0200

    feature: measure both height and inputHeight (#309)

--- packages/find-widget-worker/src/parts/MeasureInputHeight/MeasureInputHeight.ts
+++ packages/find-widget-worker/src/parts/MeasureInputHeight/MeasureInputHeight.ts
@@ -1,9 +1,26 @@
 import { measureTextHeight } from '../MeasureTextHeight/MeasureTextHeight.ts'
 
-export const measureInputHeight = (value: string, inputLineHeight: number, inputPaddingBottom: number, inputPaddingTop: number): number => {
+export interface HeightMeasurement {
+  readonly height: number
+  readonly inputHeight: number
+  readonly replaceInputHeight: number
+}
+
+export const measureInputHeight = (
+  value: string,
+  inputLineHeight: number,
+  inputPaddingBottom: number,
+  inputPaddingTop: number,
+  replaceExpanded: boolean,
+): HeightMeasurement => {
   const textHeight = measureTextHeight(value, inputLineHeight)
   const inputHeight = textHeight + inputPaddingBottom + inputPaddingTop
+  const replaceInputHeight = replaceExpanded ? inputHeight : 0 // TODO should be independent of input
   // TODO use variables for margin
-  const height = inputHeight + 5
-  return height
+  const height = inputHeight + replaceInputHeight + 5
+  return {
+    inputHeight,
+    height,
+    replaceInputHeight,
+  }
 }

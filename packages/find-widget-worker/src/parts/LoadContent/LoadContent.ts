bb8ff0c8af5cee3f916b8029d9b3301fbe258fba
Le Vivilet <levivilet@pm.me>
2025-09-12 16:53:00 +0200

    tsc

--- packages/find-widget-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/find-widget-worker/src/parts/LoadContent/LoadContent.ts
@@ -8,7 +8,6 @@ import * as GetMatchCount from '../GetMatchCount/GetMatchCount.ts'
 import * as GetSelectedText from '../GetSelectedText/GetSelectedText.ts'
 import * as GetSelections from '../GetSelections/GetSelections.ts'
 import { measureInputHeight } from '../MeasureInputHeight/MeasureInputHeight.ts'
-import { measureTextHeight } from '../MeasureTextHeight/MeasureTextHeight.ts'
 import { restoreState } from '../RestoreState/RestoreState.ts'
 
 export const loadContent = async (state: FindWidgetState, savedState?: any): Promise<FindWidgetState> => {

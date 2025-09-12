fe7877582fccd6e67e7b65e767c5339c66b4b220
Le Vivilet <levivilet@pm.me>
2025-09-12 16:30:24 +0200

    feature: render replace value

--- packages/find-widget-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/find-widget-worker/src/parts/DiffModules/DiffModules.ts
@@ -5,6 +5,7 @@ import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffFocusContext from '../DiffFocusContext/DiffFocusContext.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffParentUid from '../DiffParentUid/DiffParentUid.ts'
+import * as DiffReplaceValue from '../DiffReplaceValue/DiffReplaceValue.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 import * as DiffValue from '../DiffValue/DiffValue.ts'
 
@@ -16,6 +17,7 @@ export const modules = [
   DiffFocusContext.isEqual,
   DiffFocus.isEqual,
   DiffValue.isEqual,
+  DiffReplaceValue.isEqual,
   DiffCss.isEqual,
 ]
 
@@ -27,5 +29,6 @@ export const numbers = [
   DiffType.RenderFocusContext,
   DiffType.RenderFocus,
   DiffType.RenderValue,
+  DiffType.RenderReplaceValue,
   DiffType.RenderCss,
 ]

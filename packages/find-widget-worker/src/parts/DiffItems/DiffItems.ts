2a61d199ef54584ba0364d68bc13bb611332fbb9
Le Vivilet <levivilet@pm.me>
2025-09-13 23:09:19 +0200

    feature: rerender when error message changes (#332)

--- packages/find-widget-worker/src/parts/DiffItems/DiffItems.ts
+++ packages/find-widget-worker/src/parts/DiffItems/DiffItems.ts
@@ -2,13 +2,14 @@ import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
 
 export const isEqual = (oldState: FindWidgetState, newState: FindWidgetState): boolean => {
   return (
-    oldState.version === newState.version &&
-    oldState.replaceExpanded === newState.replaceExpanded &&
+    oldState.inputErrorMessage === newState.inputErrorMessage &&
+    oldState.matchCase === newState.matchCase &&
     oldState.matchCount === newState.matchCount &&
     oldState.matchIndex === newState.matchIndex &&
-    oldState.matchCase === newState.matchCase &&
     oldState.matchWholeWord === newState.matchWholeWord &&
+    oldState.preserveCase === newState.preserveCase &&
+    oldState.replaceExpanded === newState.replaceExpanded &&
     oldState.useRegularExpression === newState.useRegularExpression &&
-    oldState.preserveCase === newState.preserveCase
+    oldState.version === newState.version
   )
 }

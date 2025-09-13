b9772cff36212a7c3836bfec38af35d4d3a0b11b
Le Vivilet <levivilet@pm.me>
2025-09-13 15:51:22 +0200

    feature: enable replace e2e test (#316)

--- packages/e2e/src/find-widget-replace-one-occurence.ts
+++ packages/e2e/src/find-widget-replace-one-occurence.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.find-replace-one-occurrence'
 
-export const skip = 1
-
 export const test: Test = async ({ Command, FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()

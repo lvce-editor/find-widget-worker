7d2c58985574827b1cddf46b08cb1bd1f4601f37
Le Vivilet <levivilet@pm.me>
2025-09-14 08:40:32 +0200

    debug flaky test

--- packages/find-widget-worker/src/parts/FindWidgetStates/FindWidgetStates.ts
+++ packages/find-widget-worker/src/parts/FindWidgetStates/FindWidgetStates.ts
@@ -2,3 +2,13 @@ import * as ViewletRegistry from '@lvce-editor/viewlet-registry'
 import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
 
 export const { get, set, wrapCommand, dispose, getCommandIds, registerCommands, wrapGetter } = ViewletRegistry.create<FindWidgetState>()
+
+export const wrapCommand2 =
+  (fn: any): any =>
+  (...args: readonly any[]): any => {
+    // @ts-ignore
+    // eslint-disable-next-line no-console
+    console.log(`exec`, fn.name)
+    // @ts-ignore
+    return wrapCommand(fn)(...args)
+  }

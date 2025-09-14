5ec56b5d53e7abf2dd539362c59bf2b22022ec08
Le Vivilet <levivilet@pm.me>
2025-09-14 09:13:32 +0200

    feature: improve static build (#341)

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -1,7 +1,7 @@
 import { join } from 'node:path'
 import { pathToFileURL } from 'node:url'
 import { root } from './root.js'
-import { cp } from 'node:fs/promises'
+import { cp, readFile, writeFile } from 'node:fs/promises'
 
 const sharedProcessPath = join(root, 'packages', 'server', 'node_modules', '@lvce-editor', 'shared-process', 'index.js')
 
@@ -10,10 +10,29 @@ const sharedProcessUrl = pathToFileURL(sharedProcessPath).toString()
 const sharedProcess = await import(sharedProcessUrl)
 
 process.env.PATH_PREFIX = '/find-widget-worker'
-await sharedProcess.exportStatic({
+const { commitHash } = await sharedProcess.exportStatic({
   root,
   extensionPath: '',
   testPath: 'packages/e2e',
 })
+
+const rendererWorkerPath = join(root, 'dist', commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js')
+
+export const getRemoteUrl = (path) => {
+  const url = pathToFileURL(path).toString().slice(8)
+  return `/remote/${url}`
+}
+
+const content = await readFile(rendererWorkerPath, 'utf8')
+const workerPath = join(root, '.tmp/dist/dist/findWidgetWorkerMain.js')
+const remoteUrl = getRemoteUrl(workerPath)
+
+if (content.includes('// const findWidgetWorkerUrl = ')) {
+  const occurrence = `// const findWidgetWorkerUrl = \`\${assetDir}/packages/find-widget-worker/dist/findWidgetWorkerMain.js\`
+  const findWidgetWorkerUrl = \`${remoteUrl}\``
+  const replacement = `const findWidgetWorkerUrl = \`\${assetDir}/packages/find-widget-worker/dist/findWidgetWorkerMain.js\``
+  const newContent = content.replace(occurrence, replacement)
+  await writeFile(rendererWorkerPath, newContent)
+}
 
 await cp(join(root, 'dist'), join(root, '.tmp', 'static'), { recursive: true })

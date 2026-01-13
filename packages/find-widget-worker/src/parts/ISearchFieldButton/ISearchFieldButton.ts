277d3058f0de7a2c8754d1b1803ea45f24dbd0e4
Le Vivilet <levivilet@pm.me>
2026-01-13 18:02:22 +0100

    fix

--- packages/find-widget-worker/src/parts/ISearchFieldButton/ISearchFieldButton.ts
+++ packages/find-widget-worker/src/parts/ISearchFieldButton/ISearchFieldButton.ts
@@ -2,6 +2,6 @@ export interface ISearchFieldButton {
   readonly checked: boolean
   readonly icon: string
   readonly name: string
-  readonly onClick: string
+  readonly onClick: string | number
   readonly title: string
 }

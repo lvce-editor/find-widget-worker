b703a33368baae96078394ba82bdf2b3f1c3876f
Le Vivilet <levivilet@pm.me>
2026-01-13 18:27:36 +0100

    feature: add replace e2e test (#388)

---
+++ packages/e2e/src/find-widget-replace.ts
@@ -0,0 +1,21 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'find-widget-replace'
+
+export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.txt`, `a`)
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/file1.txt`)
+  await Editor.setSelections(new Uint32Array([0, 0, 0, 7]))
+  await Editor.openFindWidget()
+  await FindWidget.toggleReplace()
+  await FindWidget.setReplaceValue('b')
+
+  // act
+  await FindWidget.replace()
+
+  // assert
+  await Editor.shouldHaveText('b')
+}

9b5a2e9d33aae71ca593a4a2b1aaa5bf2bf19290
Le Vivilet <levivilet@pm.me>
2026-01-13 18:34:54 +0100

    feature: better handle empty selections (#390)

--- packages/find-widget-worker/src/parts/GetSelectedText/GetSelectedText.ts
+++ packages/find-widget-worker/src/parts/GetSelectedText/GetSelectedText.ts
@@ -1,4 +1,7 @@
 export const getSelectedText = (lines: readonly string[], selections: readonly number[]): string => {
+  if (selections.length === 0) {
+    return ''
+  }
   const startRowIndex = selections[0]
   const startColumnIndex = selections[1]
   const endColumnIndex = selections[3]

0027186d0e9327cbb6283e2c609b8ab34024465f
Le Vivilet <levivilet@pm.me>
2026-01-13 17:23:48 +0100

    feature: use page object

--- packages/e2e/src/find-widget-close.ts
+++ packages/e2e/src/find-widget-close.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'find-widget-close'
 
-export const test: Test = async ({ Command, FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
+export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(
@@ -16,7 +16,7 @@ content 2`,
   await Editor.openFindWidget()
 
   // act
-  await Command.execute(`FindWidget.close`)
+  await FindWidget.close()
 
   // assert
   const findWidget = Locator('.FindWidget')

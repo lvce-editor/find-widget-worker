ec7a924ecacdd3532fb314ca34dd09579573cb6f
Le Vivilet <levivilet@pm.me>
2026-01-13 22:20:36 +0100

    skip

--- packages/e2e/src/find-widget-replace-all-no-results.ts
+++ packages/e2e/src/find-widget-replace-all-no-results.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'find-widget-replace-all-no-results'
 
+export const skip = 1
+
 export const test: Test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect, FindWidget }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()

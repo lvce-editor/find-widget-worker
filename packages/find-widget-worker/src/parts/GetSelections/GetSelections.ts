5c1de1e37fc88e661782555216e08374c95257f6
Le Vivilet <levivilet@pm.me>
2026-01-13 17:54:39 +0100

    feature: use editor worker module

--- packages/find-widget-worker/src/parts/GetSelections/GetSelections.ts
+++ packages/find-widget-worker/src/parts/GetSelections/GetSelections.ts
@@ -1,8 +1,6 @@
 import { EditorWorker } from '@lvce-editor/rpc-registry'
 
 export const getSelections = async (editorUid: number): Promise<readonly number[]> => {
-  // @ts-ignore
-  const selections = await EditorWorker.invoke('Editor.getSelections2', editorUid)
-  // @ts-ignore
+  const selections = await EditorWorker.getSelections(editorUid)
   return selections
 }

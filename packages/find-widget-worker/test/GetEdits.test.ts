28cfe290e75300f1afbdee4370895763eef23b38
Le Vivilet <levivilet@pm.me>
2026-01-20 21:50:55 +0100

    feature: add edits test

--- packages/find-widget-worker/test/GetEdits.test.ts
+++ packages/find-widget-worker/test/GetEdits.test.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@jest/globals'
+import { findMatches } from '../src/parts/FindMatches/FindMatches.ts'
 import * as FindRegexMatches from '../src/parts/FindRegexMatches/FindRegexMatches.ts'
 import { getEdits } from '../src/parts/GetEdits/GetEdits.ts'
 import * as GetSearchRegex from '../src/parts/GetSearchRegex/GetSearchRegex.ts'
@@ -58,4 +59,60 @@ test('getEdits - no value returns empty edits', () => {
   const matches: Uint32Array = new Uint32Array([])
   const edits = getEdits(matches, value, replacement, 0, true, lines)
   expect(edits).toEqual([])
+})
+
+test('getEdits - useRegularExpression with valid regex pattern', () => {
+  const lines: readonly string[] = ['foo bar foo', 'bar foo']
+  const value: string = 'fo+'
+  const replacement: string = 'baz'
+  const options = {
+    matchCase: false,
+    matchWholeWord: false,
+    useRegularExpression: true,
+  }
+  const { matches } = findMatches(lines, value, options)
+  const edits = getEdits(matches, value, replacement, 0, false, lines, false, options)
+  expect(edits).toEqual([
+    {
+      endOffset: 3,
+      inserted: 'baz',
+      origin: 'find-widget.replace',
+      startOffset: 0,
+    },
+  ])
+})
+
+test('getEdits - useRegularExpression with invalid regex pattern', () => {
+  const lines: readonly string[] = ['foo bar foo', 'bar foo']
+  const value: string = '('
+  const replacement: string = 'baz'
+  const options = {
+    matchCase: false,
+    matchWholeWord: false,
+    useRegularExpression: true,
+  }
+  const { matches } = findMatches(lines, value, options)
+  const edits = getEdits(matches, value, replacement, 0, false, lines, false, options)
+  expect(edits).toEqual([])
+})
+
+test('getEdits - useRegularExpression with back-reference replacement', () => {
+  const lines: readonly string[] = ['hello world', 'foo bar']
+  const value: string = '(\\w+) (\\w+)'
+  const replacement: string = '$2 $1'
+  const options = {
+    matchCase: false,
+    matchWholeWord: false,
+    useRegularExpression: true,
+  }
+  const { matches } = findMatches(lines, value, options)
+  const edits = getEdits(matches, value, replacement, 0, false, lines, false, options)
+  expect(edits).toEqual([
+    {
+      endOffset: 11,
+      inserted: 'world hello',
+      origin: 'find-widget.replace',
+      startOffset: 0,
+    },
+  ])
 })

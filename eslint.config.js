01fa8e1896479c0636c46e01128c4e41c69a0559
Le Vivilet <levivilet@pm.me>
2026-06-18 23:49:32 +0200

    chore: update eslint spellchecker words

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,8 @@
+import cspellPlugin from '@cspell/eslint-plugin'
 import * as config from '@lvce-editor/eslint-config'
 import * as actions from '@lvce-editor/eslint-plugin-github-actions'
+
+const cspellOptions = config.default.find((item) => item.rules?.['@cspell/spellchecker'])?.rules['@cspell/spellchecker'][1]
 
 export default [
   ...config.default,
@@ -14,6 +17,24 @@ export default [
       '@typescript-eslint/prefer-readonly-parameter-types': 'off',
       'no-useless-escape': 'off',
       'no-restricted-syntax': 'off',
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      '@cspell': cspellPlugin,
+    },
+    rules: {
+      '@cspell/spellchecker': [
+        'error',
+        {
+          ...cspellOptions,
+          cspell: {
+            ...cspellOptions.cspell,
+            words: [...cspellOptions.cspell.words, 'barfoo', 'barfoobar', 'Iconreplace', 'offscreencanvas'],
+          },
+        },
+      ],
     },
   },
 ]

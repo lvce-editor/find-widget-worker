6a316aeba01d55c23cc3e547fac8262dc729ddf8
Le Vivilet <levivilet@pm.me>
2026-07-17 19:34:13 +0000

    fix: navigate to previous match with shift enter

--- packages/find-widget-worker/test/GetKeyBindings.test.ts
+++ packages/find-widget-worker/test/GetKeyBindings.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@jest/globals'
-import { KeyCode, WhenExpression } from '@lvce-editor/constants'
+import { KeyCode, KeyModifier, WhenExpression } from '@lvce-editor/constants'
 import * as GetKeyBindings from '../src/parts/GetKeyBindings/GetKeyBindings.ts'
 
 test('getKeyBindings - returns an array of key bindings', () => {
@@ -13,6 +13,16 @@ test('getKeyBindings - Enter focuses the next match from the find input', () => 
   expect(result).toContainEqual({
     command: 'FindWidget.focusNext',
     key: KeyCode.Enter,
+    when: WhenExpression.FocusFindWidget,
+  })
+})
+
+test('getKeyBindings - Shift+Enter focuses the previous match from the find input', () => {
+  const result = GetKeyBindings.getKeyBindings()
+
+  expect(result).toContainEqual({
+    command: 'FindWidget.focusPrevious',
+    key: KeyModifier.Shift | KeyCode.Enter,
     when: WhenExpression.FocusFindWidget,
   })
 })

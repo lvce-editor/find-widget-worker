000ca9673c4d3f35ace986dd947cdebcc8a86eac
Le Vivilet <levivilet@pm.me>
2026-07-19 15:06:38 +0200

    feature: enable recommended virtual dom lint rules (#432)

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
 export default [
   ...config.default,
+  ...config.recommendedVirtualDom,
   ...actions.default,
   {
     ignores: ['**/server/**', '**/e2e/**', '**/memory/**'],
@@ -14,6 +15,23 @@ export default [
       '@typescript-eslint/prefer-readonly-parameter-types': 'off',
       'no-useless-escape': 'off',
       'no-restricted-syntax': 'off',
+    },
+  },
+  {
+    files: ['packages/find-widget-worker/src/parts/**/*.ts'],
+    ignores: ['packages/find-widget-worker/src/parts/**/*VirtualDom/**/*.ts'],
+    rules: {
+      'virtual-dom/prefer-state-destructuring': 'off',
+    },
+  },
+  {
+    files: ['packages/find-widget-worker/test/**/*.ts'],
+    rules: {
+      'virtual-dom/no-inline-event-handlers': 'off',
+      'virtual-dom/prefer-constants': 'off',
+      'virtual-dom/prefer-merge-class-names': 'off',
+      'virtual-dom/prefer-state-destructuring': 'off',
+      'virtual-dom/valid-child-count': 'off',
     },
   },
 ]

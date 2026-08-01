efee00372b2cad4b315993c499f4eec50a2bfd7b
Le Vivilet <levivilet@pm.me>
2026-08-01 07:03:43 -0700

    Fix find widget replacement actions (#441)

--- packages/find-widget-worker/src/parts/RenderFocusContext/RenderFocusContext.ts
+++ packages/find-widget-worker/src/parts/RenderFocusContext/RenderFocusContext.ts
@@ -1,7 +1,14 @@
-import { ViewletCommand } from '@lvce-editor/constants'
+import { ViewletCommand, WhenExpression as SearchWhenExpression } from '@lvce-editor/constants'
 import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
 import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
 export const renderFocusContext = (oldState: FindWidgetState, newState: FindWidgetState): readonly any[] => {
-  return [/* method */ ViewletCommand.SetFocusContext, WhenExpression.FocusFindWidget]
+  const { focus } = newState
+  if (focus === SearchWhenExpression.FocusSearchReplaceInput) {
+    return [/* method */ ViewletCommand.SetFocusContext, SearchWhenExpression.FocusFindWidgetReplace]
+  }
+  if (focus === SearchWhenExpression.FocusSearchInput || !focus) {
+    return [/* method */ ViewletCommand.SetFocusContext, WhenExpression.FocusFindWidget]
+  }
+  return [/* method */ ViewletCommand.SetFocusContext, focus]
 }

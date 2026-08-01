8468d61e2f2d5b20ba2800898953a9d3e53ac7e4
Le Vivilet <levivilet@pm.me>
2026-08-01 20:44:53 +0000

    fix: hoist replacement test regexes

--- packages/find-widget-worker/test/ApplyRegexReplacement.test.ts
+++ packages/find-widget-worker/test/ApplyRegexReplacement.test.ts
@@ -1,14 +1,18 @@
 import { expect, test } from '@jest/globals'
 import { applyRegexReplacement } from '../src/parts/ApplyRegexReplacement/ApplyRegexReplacement.ts'
 
+const helloCaptureRegex = /(hello)/g
+const optionalACaptureRegex = /(a)?b/g
+const worldCaptureRegex = /(world)/g
+
 test('returns the replacement when the regular expression does not match', () => {
-  expect(applyRegexReplacement('hello', /(world)/g, '$1')).toBe('$1')
+  expect(applyRegexReplacement('hello', worldCaptureRegex, '$1')).toBe('$1')
 })
 
 test('replaces an unmatched optional capture group with an empty string', () => {
-  expect(applyRegexReplacement('b', /(a)?b/g, '[$1]')).toBe('[]')
+  expect(applyRegexReplacement('b', optionalACaptureRegex, '[$1]')).toBe('[]')
 })
 
 test('preserves a reference to a capture group that does not exist', () => {
-  expect(applyRegexReplacement('hello', /(hello)/g, '$2')).toBe('$2')
+  expect(applyRegexReplacement('hello', helloCaptureRegex, '$2')).toBe('$2')
 })

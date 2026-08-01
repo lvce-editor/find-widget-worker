cd111ccd887818c2be10d3bb3f6f1bb5982b31ae
Le Vivilet <levivilet@pm.me>
2026-08-01 15:25:11 +0000

    fix: avoid refocusing user-focused controls

--- packages/find-widget-worker/src/parts/DiffFocus/DiffFocus.ts
+++ packages/find-widget-worker/src/parts/DiffFocus/DiffFocus.ts
@@ -1,5 +1,9 @@
 import type { FindWidgetState } from '../FindWidgetState/FindWidgetState.ts'
+import * as FocusSource from '../FocusSource/FocusSource.ts'
 
 export const isEqual = (oldState: FindWidgetState, newState: FindWidgetState): boolean => {
+  if (newState.focusSource === FocusSource.User) {
+    return true
+  }
   return oldState.focused === newState.focused && oldState.focus === newState.focus && oldState.focusVersion === newState.focusVersion
 }

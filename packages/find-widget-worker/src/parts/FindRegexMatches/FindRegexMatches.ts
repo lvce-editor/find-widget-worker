66e40e501ec3c3d8dabcab6fddaf9415c74db19a
Le Vivilet <levivilet@pm.me>
2026-09-22 22:23:34 +0200

    feature: simplify findMatches

--- packages/find-widget-worker/src/parts/FindRegexMatches/FindRegexMatches.ts
+++ packages/find-widget-worker/src/parts/FindRegexMatches/FindRegexMatches.ts
@@ -1,19 +1,20 @@
+const collectMatchesForLineInternal = (matches: number[], line: string, lineIndex: number): void => {
+  let lastMatch = regex.exec(line)
+  while (lastMatch) {
+    matches.push(lineIndex, lastMatch.index, lastMatch[0].length)
+    lastMatch = regex.exec(line)
+  }
+}
+
 export const findRegexMatches = (lines: readonly string[], regex: RegExp): Uint32Array => {
   if (!regex.global) {
     throw new Error('regex must be global')
   }
   const { length } = lines
   const matches: number[] = []
-  const collectMatchesForLine = (line: string, lineIndex: number): void => {
-    let lastMatch = regex.exec(line)
-    while (lastMatch) {
-      matches.push(lineIndex, lastMatch.index, lastMatch[0].length)
-      lastMatch = regex.exec(line)
-    }
-  }
 
   for (let i = 0; i < length; i++) {
-    collectMatchesForLine(lines[i], i)
+    collectMatchesForLineInternal(matches, lines[i], i)
   }
 
   return new Uint32Array(matches)

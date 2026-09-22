aed01b7c8c06e770d1da75e7f31d0098dfc8b4ab
Le Vivilet <levivilet@pm.me>
2026-09-22 03:39:43 -0700

    fix: resize find widget with editor (#460)

--- packages/find-widget-worker/src/parts/GetFindWidgetPosition/GetFindWidgetPosition.ts
+++ packages/find-widget-worker/src/parts/GetFindWidgetPosition/GetFindWidgetPosition.ts
@@ -1,7 +1,6 @@
 import type { Bounds } from '../Bounds/Bounds.ts'
 
-export const getFindWidgetPosition = (editorX: number, editorY: number, editorWidth: number): Bounds => {
-  const width = 300
+export const getFindWidgetPosition = (editorX: number, editorY: number, editorWidth: number, width = 300): Bounds => {
   const height = 30
   const paddingTop = 10
   const paddingRight = 20

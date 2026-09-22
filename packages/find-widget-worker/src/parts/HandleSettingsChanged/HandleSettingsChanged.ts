ea46f52dc223f8e18c9cf74457891fef2bf90597
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2026-09-22 12:51:52 +0000

    style: format settings change handler

--- packages/find-widget-worker/src/parts/HandleSettingsChanged/HandleSettingsChanged.ts
+++ packages/find-widget-worker/src/parts/HandleSettingsChanged/HandleSettingsChanged.ts
@@ -3,15 +3,7 @@ import * as GetFontSize from '../GetFontSize/GetFontSize.ts'
 import { measureInputHeight } from '../MeasureInputHeight/MeasureInputHeight.ts'
 
 export const handleSettingsChanged = async (state: FindWidgetState): Promise<FindWidgetState> => {
-  const {
-    inputBorderWidth,
-    inputGap,
-    inputLineHeight,
-    inputPaddingBottom,
-    inputPaddingTop,
-    replaceExpanded,
-    value,
-  } = state
+  const { inputBorderWidth, inputGap, inputLineHeight, inputPaddingBottom, inputPaddingTop, replaceExpanded, value } = state
   const fontSize = await GetFontSize.getFontSize()
   const { height, inputHeight } = measureInputHeight(
     value,
